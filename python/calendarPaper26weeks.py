74632a8ffa03e3a0aae5823601847d055655e953
Dave Jagoda <davejagoda@gmail.com>
2012-09-26 16:57:33 -0700

    add calendarPaper26weeks.py

---
+++ python/calendarPaper26weeks.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys, datetime
+
+def printHeader():
+    print """<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Calendar Table</title>
+<style type="text/css">
+  table{
+    border: 1px solid black;
+    width: 8.5in;
+    table-layout: fixed;
+  }
+  th{
+    height: .1in;
+  }
+  td{
+    border: 1px solid black;
+    height: .36in;
+    vertical-align: top;
+    text-align:right;
+  }
+</style>
+</head>
+<body>
+<table>
+<thead>
+  <tr>
+    <th>日</th>
+    <th>月</th>
+    <th>火</th>
+    <th>水</th>
+    <th>木</th>
+    <th>金</th>
+    <th>土</th>
+  </tr>
+</thead>
+<tbody>
+"""
+
+def printFooter():
+    print """</tbody>
+</table>
+</body>
+</html>
+"""
+
+def getSundayStartDate(d):
+    shiftDays = - ( (d.weekday() + 1) % 7)
+#    print shiftDays
+    return d + shiftDays * datetime.timedelta(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        print('error: maximum 1 argument')
+        sys.exit(1)
+    if len(sys.argv) == 2:
+        # one argument provided, check if it's a date
+        # in the format CCYY-MM-DD
+        yy = int(sys.argv[1][0:4])
+        mm = int(sys.argv[1][4:6])
+        dd = int(sys.argv[1][6:8])
+#        print yy, mm, dd
+        startDate = datetime.date(yy, mm, dd)
+    else:
+        startDate = datetime.date.today()
+
+    currDate = getSundayStartDate(startDate)
+    printHeader()
+    for i in xrange(26*7):
+        if i % 7 == 0:
+            print "  <tr>"
+        print "    <td>",
+        if i == 0:
+            print currDate.year, currDate.strftime('%b'), currDate.day,
+        else:
+            if currDate.month == 1 and currDate.day == 1:
+                print currDate.year,
+            if currDate.day == 1:
+                print currDate.strftime('%b'),
+            print currDate.day,
+        print "</td>"
+        if i % 7 == 6:
+            print "  </tr>"
+        currDate = currDate + datetime.timedelta(1)
+
+    printFooter()
