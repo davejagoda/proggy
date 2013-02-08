b324a143524d294f02badd7d8636e3f4b4d07f56
Dave Jagoda <davejagoda@gmail.com>
2013-02-08 15:17:01 -0800

    Commit for Xcode with Github

---
+++ Proggy/Proggy/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Proggy
+//
+//  Created by dj on 2/8/13.
+//  Copyright (c) 2013 dj. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
