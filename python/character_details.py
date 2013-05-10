8b89704dff41bbfcb05cf4657a8f6ae93a6bc8ea
dj <davejagoda@gmail.com>
2013-05-10 16:16:10 +0000

    change print to print()

--- python/character_details.py
+++ python/character_details.py
@@ -3,7 +3,7 @@
 import sys, unicodedata
 
 for arg in sys.argv[1:]:
-    print 'argument:',  arg, 'argument length', len(arg)
+    print('argument: ' + arg + ' argument length ' + str(len(arg)))
     unicode_string = arg.decode('utf-8')
     for u in unicode_string:
-        print u, ord(u), hex(ord(u))
+        print(u + ' ' + str(ord(u)) + ' ' + hex(ord(u)))
