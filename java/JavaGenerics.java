b24cf510d502f376a7da9557498ebd78c5959c1c
Dave Jagoda <davejagoda@gmail.com>
2013-05-18 17:30:26 -0700

    make JavaGenerics.java actually compile

--- java/JavaGenerics.java
+++ java/JavaGenerics.java
@@ -1,17 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class JavaGenerics {
     public static void main(String[] args) {
-	System.out.println("Generics");
+	System.out.println("Generics, in main");
+	WithoutGenerics();
+	WithGenerics();
+	System.out.println("Generics, about to finish");
+    }
 // from this page:
 // http://en.wikipedia.org/wiki/Generics_in_Java
 
 // without generics
+    public static void WithoutGenerics() {
+	System.out.println("Generics, in WithoutGenerics");
 	List v = new ArrayList();
 	v.add("test");
-	Integer i = (Integer)v.get(0);        // Run time error
+// uncomment the next line to get the runtime error
+//	Integer i = (Integer)v.get(0);        // Run time error
+    }
 
 // with generics
+    public static void WithGenerics() {
+	System.out.println("Generics, in WithGenerics");
 	List<String> v = new ArrayList<String>();
 	v.add("test");
-	Integer i = v.get(0); // (type error)  Compile time error
+// uncomment the next line to get the compile time error
+//	Integer i = v.get(0); // (type error)  Compile time error
     }
 }
