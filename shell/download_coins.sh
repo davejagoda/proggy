bcc034c54515a426877e4708e35b60df4767b134
dj <davejagoda@gmail.com>
2013-05-31 15:19:59 -0700

    add download_coins.sh

---
+++ shell/download_coins.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+curl -O http://www.usmint.gov/mint_programs/info/images/2012_cent_unc_obv_200.jpg
+curl -O http://www.usmint.gov/mint_programs/info/images/2012_nickel_unc_obv_200.jpg
+curl -O http://www.usmint.gov/mint_programs/info/images/2012_dime_unc_obv_200.jpg
+curl -O http://www.usmint.gov/mint_programs/info/images/2011-Unc-Quarter-obv-D.jpg
+curl -O http://www.usmint.gov/mint_programs/info/images/2012_halfdollar_unc_obv_200.jpg
+curl -O http://www.usmint.gov/mint_programs/info/images/2011-Unc-\$1-reverse.jpg
