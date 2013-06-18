67bbbcb25affaeb9e0bd1d1902d3d409f2722784
dj <davejagoda@gmail.com>
2013-06-18 10:41:50 -0700

    reformat it to see if it looks better

--- lisp/tshirt.rkt
+++ lisp/tshirt.rkt
@@ -1,3 +1,6 @@
 #lang racket
-(define s '(a n d r e e s s e n h o r o w i t z))
-(list (car s) (length (cdr (cdr s))) (last s))
+(define s
+'(a n d r e e s s e n h o r o w i t z))
+(list (car s)
+      (length (cdr (cdr s)))
+      (last s))
