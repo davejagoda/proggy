0635e315ce29ccd38d7fde0d64af1d4bfe531858
Dave Jagoda <davejagoda@gmail.com>
2013-09-09 16:53:46 -0700

    add commandLineTweet.py

---
+++ python/commandLineTweet.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import os, sys, tweepy
+
+tokens=[
+    'ConsumerKey',
+    'ConsumerSecret',
+    'AccessTokenKey',
+    'AccessTokenSecret'
+    ]
+
+for token in tokens:
+    if not os.getenv(token):
+        print 'error: set', token, 'environment variable'
+        sys.exit(1)
+
+if len(sys.argv) != 2:
+    print 'Usage:', sys.argv[0], '<message to tweet>'
+    sys.exit(1)
+
+auth = tweepy.OAuthHandler(os.getenv('ConsumerKey'),
+                           os.getenv('ConsumerSecret'))
+auth.set_access_token(os.getenv('AccessTokenKey'),
+                      os.getenv('AccessTokenSecret'))
+api = tweepy.API(auth)
+try:
+    api.update_status(sys.argv[1])
+    print 'tweeted:', sys.argv[1], 'as user:', api.me().name
+except:
+    print 'tweet failed'
+
