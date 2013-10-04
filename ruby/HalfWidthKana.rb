7ca43ed2a51a752a6d7d2b7ae6644863413a0d14
Dave Jagoda <davejagoda@gmail.com>
2013-10-03 17:52:29 -0700

    for vs. each vs. upto, what is the most rubonic?

--- ruby/HalfWidthKana.rb
+++ ruby/HalfWidthKana.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/ruby
 
 #for i in (0xff65..0xff9f)
-(0xff65..0xff9f).each do |i|
-    print i.to_s(16), ":", [i.to_s(16).to_i(16)].pack("U*"), "\n"
-end
+#(0xff65..0xff9f).each do |i|
+0xff65.upto(0xff9f) { |i| print i.to_s(16), ":", [i.to_s(16).to_i(16)].pack("U*"), "\n" }
