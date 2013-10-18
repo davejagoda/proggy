d5d4fda3daf73c93d8946db5612cce27ee622a5d
Dave Jagoda <davejagoda@gmail.com>
2013-10-18 11:02:03 -0700

    add integerOverflow.js and integerUnderflow.js

---
+++ javascript/integerOverflow.js
@@ -0,0 +1,10 @@
+#!/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc
+
+i = 1;
+j = 1;
+
+while (j !== Infinity) {
+    j = j * 2;
+    print(i, j);
+    i++;
+}
---
+++ javascript/integerUnderflow.js
@@ -0,0 +1,9 @@
+#!/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc
+
+i = 1;
+j = 1;
+while ( j !== 0 ) {
+    j = j / 2;
+    print(i, j);
+    i++;
+}
