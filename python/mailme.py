061cfd0894aed629773d255a1da5d396c04adc0a
Dave Jagoda <davejagoda@gmail.com>
2013-12-19 23:02:06 +0000

    add mailme.py

---
+++ python/mailme.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys, smtplib
+assert (2 == len(sys.argv)), "provide email address as an argument"
+addr = sys.argv[1]
+m = "To: " + addr + "\n"
+m += "From: " + addr + "\n"
+m += "Subject: test from " + sys.argv[0] + "\n"
+m += "Hi\n"
+
+s = smtplib.SMTP('localhost')
+s.sendmail(addr, [addr], m)
+s.quit()
