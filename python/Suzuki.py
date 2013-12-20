f9bc5d357c1ef132b24e9b266354c40795b593f9
Dave Jagoda <davejagoda@gmail.com>
2013-12-20 00:37:57 +0000

    add python/Suzuki.py ruby/Suzuki.rb

---
+++ python/Suzuki.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+
+print unichr(0x9234).encode('utf8') + unichr(0x6728).encode('utf8')
---
+++ ruby/Suzuki.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/ruby
+
+puts ["9234".to_i(16)].pack("U*") + ["6728".to_i(16)].pack("U*")
