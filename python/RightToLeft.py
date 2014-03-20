21b5fbceac7d0a8b434fff8df7251d184bf31e3e
Dave Jagoda <davejagoda@gmail.com>
2014-03-20 07:11:43 -0700

    add RightToLeft.py

---
+++ python/RightToLeft.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+rtl = unichr(0x202e).encode('utf8')
+ltr = unichr(0x202d).encode('utf8')
+pop = unichr(0x202c).encode('utf8')
+
+print 'hi', rtl, 'hi', ltr, 'hi'
+print 'hi', rtl, 'hi', pop, 'hi'
+
