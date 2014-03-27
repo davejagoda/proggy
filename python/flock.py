187b708bca5fae68101a9489061fa40a6456251c
Dave Jagoda <davejagoda@gmail.com>
2014-03-27 12:59:06 -0700

    add flock.py

---
+++ python/flock.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os, sys
+
+lockfile = '.flock'
+
+try:
+    f = os.open(lockfile, os.O_CREAT|os.O_EXCL)
+except:
+    print('failed to lock')
+    sys.exit(1)
+
+raw_input('press a key to release lock and exit')
+
+os.close(f)
+os.unlink(lockfile)
