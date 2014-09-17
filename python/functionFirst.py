c6419b5755925bdfe48499f39c97cc3bfca64b9d
Dave Jagoda <davejagoda@gmail.com>
2014-09-17 13:59:01 -0700

    add functionFirst.py

---
+++ python/functionFirst.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+# Python requires the function to be defined first
+
+def fun():
+    print("inside function fun")
+
+if '__main__' == __name__:
+    fun()
+    print("hello function first")
