932ce9f081a6f7e7948c3352005c2a2c01bf0ce5
Dave Jagoda <davejagoda@gmail.com>
2014-11-21 12:07:47 -0800

    print out unicode name when a non-ASCII character is found

--- python/notASCII.py
+++ python/notASCII.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-import sys, string, codecs
+import sys, string, codecs, unicodedata
 
 for file in sys.argv[1:]:
     f = codecs.open(file, 'r', 'utf8')
     for datum in f.read():
         if datum not in string.printable:
-            print(datum.encode('utf8'))
+            print(datum.encode('utf8') + ':' + unicodedata.name(datum))
     f.close()
