d10d72a69128b2139335acb7502acd0c8416076f
Dave Jagoda <davejagoda@gmail.com>
2014-12-19 09:35:08 -0800

    allow TalkingThatForeign.py to take an argument about what language locales not to talk

--- python/TalkingThatForeign.py
+++ python/TalkingThatForeign.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
-import subprocess, re
+import subprocess, sys, re
 
 stdout = subprocess.check_output(['say', '-v', '?'])
+
+if (2 == len(sys.argv)):
+    pattern = sys.argv[1]
+else:
+    pattern = 'en_US'
 
 for line in stdout.split('\n'):
     if len(line) > 0:
         (left,right) = line.split('#')
-        if (None == re.search('en_US', left)):
+        if (None == re.search(pattern, left)):
             leftsplit=left.split(' ');
             print leftsplit[0], right
             subprocess.call(['say', '-v', leftsplit[0], right])
