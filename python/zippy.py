71c0b5f8631b2dc052d390c7ede328a24b9c6ac4
Dave Jagoda <davejagoda@gmail.com>
2015-02-28 15:00:23 -0800

    add zippy.py

---
+++ python/zippy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os, httplib, time
+
+userId = os.getenv('USPSID')
+srvr = 'production.shippingapis.com'
+
+for i in xrange(10**5):
+    zip = str(i).zfill(5)
+    path = '/ShippingAPITest.dll?API=CityStateLookup&XML=<CityStateLookupRequest%20USERID="' +\
+           userId +\
+           '"><ZipCode%20ID="0"><Zip5>' +\
+           zip +\
+           '</Zip5></ZipCode></CityStateLookupRequest>'
+
+    print(path)
+    conn = httplib.HTTPConnection(srvr)
+    conn.request('GET', path)
+    resp = conn.getresponse()
+    print('status:{} reason:{} data:{}'.format(resp.status, resp.reason, resp.read()))
+    conn.close()
+    time.sleep(60)
