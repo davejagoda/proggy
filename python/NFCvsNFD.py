958f8d3f8c4297f6b459b80fed0c7a13079b929b
Dave Jagoda <davejagoda@gmail.com>
2015-03-10 09:10:21 -0700

    add NFCvsNFD.py

---
+++ python/NFCvsNFD.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Linux file systems:
+# give back whatever you receive
+# when creating, make NFC files
+
+# Apple file systems:
+# give back NFD whatever you receive
+# when creating, make NFC files
+
+import os
+
+def isAscii(s):
+    for c in s:
+        if (ord(c) > 127):
+            return(False)
+    return(True)
+
+def Spanish():
+# José vs Jose + combining accent
+    with open('José', 'a') as f:
+        f.write('José')
+        f.write('\n')
+    jose = 'Jose' + unichr(0x301)
+    with open(jose, 'a') as f:
+        f.write(jose.encode('utf8'))
+        f.write('\n')
+
+def catalog(path):
+    print(path)
+    for root, dirs, files in os.walk(path):
+        print 'root', root, 'dirs', dirs, 'files', files
+        for entry in files:
+            if (isAscii(entry)):
+                pass
+            else:
+                print('notAlpha', entry)
+
+if '__main__' == __name__:
+    Spanish()
+    catalog('.')
