b389ac0499b766cf43f78b9fd7b6c8b52fdfef13
Dave Jagoda <davejagoda@gmail.com>
2015-03-27 07:21:10 -0700

    s/argment/argument/g and change double quotes to single quotes

--- python/argparsy.py
+++ python/argparsy.py
@@ -2,11 +2,12 @@
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo the string you use here")
-parser.add_argument("-v", "--verbose", action="store_true", help="show verbose output")
+parser.add_argument('echo', help='echo the string you use here')
+parser.add_argument('-v', '--verbose', action='store_true', help='show verbose output')
 args = parser.parse_args()
 if args.verbose:
-    print "about to echo argments"
+    print 'about to echo arguments'
 print args.echo
 if args.verbose:
-    print "just echoed argments"
+    print 'just echoed arguments'
+
