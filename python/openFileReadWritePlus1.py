b6fe59123f4043989394204efdbb7c1f507c25e2
Dave Jagoda <davejagoda@gmail.com>
2015-04-25 07:40:54 -0700

    use a filename for sys.argv[1] in openFileReadWritePlus1.py

--- python/openFileReadWritePlus1.py
+++ python/openFileReadWritePlus1.py
@@ -6,7 +6,8 @@ if 2 != len(sys.argv):
     print('need exactly one argument - a file to destroy')
     sys.exit(1)
 
-with open(sys.argv[1], 'r+b') as f:
+filename = sys.argv[1]
+with open(filename, 'r+b') as f:
     octets = f.read()
     f.seek(0)
     for octet in octets:
