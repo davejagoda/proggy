b324e21b36ff242f51e46d9f0cc0b67e24eae295
Dave Jagoda <davejagoda@gmail.com>
2015-05-13 14:25:06 -0700

    treat argument as a float not an int in iso8601.py

--- python/iso8601.py
+++ python/iso8601.py
@@ -5,6 +5,6 @@ import datetime, sys
 if 1 == len(sys.argv):
     epoch = datetime.datetime.utcnow()
 else:
-    epoch = datetime.datetime.fromtimestamp(int(sys.argv[1]))
+    epoch = datetime.datetime.fromtimestamp(float(sys.argv[1]))
 
 print(epoch.replace(microsecond=0).isoformat()+'Z')
