dfe30344a89694b522966694fefc2946c24a94e2
Dave Jagoda <davejagoda@gmail.com>
2015-08-18 16:57:10 -0700

    add ability to specify useragent to requestBeauty.py

--- python/requestBeauty.py
+++ python/requestBeauty.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
+# examples:
+# ./requestBeauty.py -s https://sccl.bibliocommons.com/user/login -u <USERNAME> -p <PIN> -l 'Logged in as '
+
 import argparse, requests, bs4, re
 
-def getSess(url, username, password, verbose=False):
+def getSess(url, username, password, useragent, verbose=False):
+    if useragent:
+        headers = {'User-Agent': useragent}
+    else:
+        headers = {'User-Agent': 'Mozilla/5.0'}
     data=[]
     s = requests.Session()
-    r = s.get(url)
+    r = s.get(url, headers=headers)
     if verbose:
         print(r.status_code)
+        print(r.headers)
         print(r.content)
     soup = bs4.BeautifulSoup(r.content)
     if verbose:
@@ -16,7 +24,7 @@ def getSess(url, username, password, verbose=False):
         if 'post' == f['method']:
             action = f['action']
             if verbose:
-                print('ACTION:',f['action'])
+                print('ACTION:',action)
             for c in f.find_all('input'):
                 try:
                     value = c['value']
@@ -31,10 +39,15 @@ def getSess(url, username, password, verbose=False):
                 data.append((c['name'],value))
     return(s, action, data)
 
-def postSess(s, url, data, loggedInString, verbose=False):
-    r = s.post(url, data=data)
+def postSess(s, url, data, loggedInString, useragent, verbose=False):
+    if useragent:
+        headers = {'User-Agent': useragent}
+    else:
+        headers = {'User-Agent': 'Mozilla/5.0'}
+    r = s.post(url, headers=headers, data=data)
     if verbose:
         print(r.status_code)
+        print(r.headers)
         print(r.content)
     soup = bs4.BeautifulSoup(r.content)
     return(soup.find(text=re.compile(loggedInString)) is not None)
@@ -45,15 +58,16 @@ if '__main__' == __name__:
     parser.add_argument('-u', '--username', required=True)
     parser.add_argument('-p', '--password', required=True)
     parser.add_argument('-l', '--loggedInString', required=True)
+    parser.add_argument('-a', '--userAgentString')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
     if args.verbose:
         print(args.site, args.username, args.password)
-    (s, action, data) = getSess(args.site, args.username, args.password, verbose=args.verbose)
+    (s, action, data) = getSess(args.site, args.username, args.password, args.userAgentString, verbose=args.verbose)
     if args.verbose:
         print(action)
         print(data)
-    if (postSess(s, action, data, args.loggedInString, verbose=args.verbose)):
+    if postSess(s, action, data, args.loggedInString, args.userAgentString, verbose=args.verbose):
         print('logged in')
     else:
         print('not logged in')
