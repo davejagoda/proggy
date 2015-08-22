c3db1389f8469b4f1762fc1736e7e32c2b84094d
Dave Jagoda <davejagoda@gmail.com>
2015-08-22 10:11:15 -0700

    look for 'value out', handle titles in utf8, use 'format' with 'print' in BeautifulSCCL.py

--- python/BeautifulSCCL.py
+++ python/BeautifulSCCL.py
@@ -22,7 +22,7 @@ def loginAndReturnSoup(u, p):
 #    print(soup.prettify().encode('utf8'))
     logged_in_user = soup.find(text=re.compile('Logged in as '))
     try:
-        print('User: ' + logged_in_user)
+        print('User: {}'.format(logged_in_user))
     except:
         print('Bad password')
         sys.exit(1)
@@ -31,35 +31,40 @@ def loginAndReturnSoup(u, p):
 def displayCheckedOut(soup):
 #   <a class="jacketCoverLink" href="/item/show/973973016_office_space" target="_parent" title="Office Space">
     for c in soup.find_all('a', 'jacketCoverLink'):
+        title = None
+        for kid in c.children:
+# find the title of the child, if there's more than one child use the last one
+            title = kid['title'].encode('utf8')
         d = c.parent
 #       <span class="value overdue">
 #       <span class="value coming_due">
+#       <span class="value out">
         out = d.find_next('span', 'value overdue')
-#        print('value overdue', out)
         if out == None:
             out = d.find_next('span', 'value coming_due')
-#            print('value comedue', out)
-        print('UNIT: ' + out.get_text().strip() + ' ' +  c['title'])
+        if out == None:
+            out = d.find_next('span', 'value out')
+        print('UNIT: {} {}'.format(out.get_text().strip(), title))
 
 def fines():
     r = OPENER.open(FINESURL)
     soup = BeautifulSoup(r.read())
     fine = soup.find(text=re.compile('\$'))
-    print('Fine: ' + fine)
+    print('Fine: {}'.format(fine))
 
 def logout():
     r = OPENER.open(LOGOUTURL)
 
 if '__main__' == __name__:
     if len(sys.argv) < 2:
-        print('Usage: ' + sys.argv[0] + ' username [username ...]')
+        print('Usage: {} username [username ...]'.format(sys.argv[0]))
         sys.exit(1)
     if os.getenv('SCCLPIN'):
         password = os.getenv('SCCLPIN')
     else:
         password = getpass.getpass()
     for username in sys.argv[1:]:
-        print('USER: ' + username)
+        print('USER: {}'.format(username))
         soup = loginAndReturnSoup(username, password)
         displayCheckedOut(soup)
         fines()
