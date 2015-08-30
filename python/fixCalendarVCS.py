63b31aadda877aa7e63eca8049d51c3de63d137a
David Jagoda <davejagoda@gmail.com>
2015-08-29 22:35:35 -0700

    add fixCalendarVCS.py - this fixes the United Airlines VCS files that do not work well with Google Calendar

---
+++ python/fixCalendarVCS.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys, re
+
+if 2 != len(sys.argv):
+    print('need exactly one argument - a VCS calendar file to fix')
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+with open(filename, 'r') as f:
+    lines = f.readlines()
+
+with open(filename, 'w') as f:
+    for line in lines:
+        match = re.search('^End:(.*)$', line)
+        if match:
+            f.write('END:' + match.group(1) + '\n')
+        else:
+            f.write(line)
