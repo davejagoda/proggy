d641680da3df74edc924a42e0f053174705ae16f
Dave Jagoda <davejagoda@gmail.com>
2015-09-08 15:19:05 -0700

    update requestBeauty.py to handle inputs with no 'name' attribute and to set an HTTP Referer

--- python/requestBeauty.py
+++ python/requestBeauty.py
@@ -3,7 +3,7 @@
 # examples:
 # ./requestBeauty.py -s https://sccl.bibliocommons.com/user/login -u <USERNAME> -p <PIN> -l 'Logged in as '
 
-import argparse, requests, bs4, re
+import argparse, requests, bs4, re, urlparse
 
 def getSess(url, username, password, useragent, verbose=False):
     if useragent:
@@ -31,12 +31,18 @@ def getSess(url, username, password, useragent, verbose=False):
                 except:
                     value = ''
                 if verbose:
-                    print(c['name'],c['type'], value.encode('utf8'))
+                    try:
+                        print(c['name'],c['type'], value.encode('utf8'))
+                    except:
+                        print('NO NAME',c['type'], value.encode('utf8'))
                 if 'text' == c['type']:
                     value = username
                 if 'password' == c['type']:
                     value = password
-                data.append((c['name'],value))
+                try:
+                    data.append((c['name'],value))
+                except:
+                    print('no name exception:{}'.format(c))
     return(s, action, data)
 
 def postSess(s, url, data, loggedInString, useragent, verbose=False):
@@ -44,6 +50,11 @@ def postSess(s, url, data, loggedInString, useragent, verbose=False):
         headers = {'User-Agent': useragent}
     else:
         headers = {'User-Agent': 'Mozilla/5.0'}
+    headers['Referer'] = url
+    if verbose:
+        print('url:{}'.format(url))
+        print('headers:{}'.format(headers))
+        print('data:{}'.format(data))
     r = s.post(url, headers=headers, data=data)
     if verbose:
         print(r.status_code)
@@ -67,6 +78,10 @@ if '__main__' == __name__:
     if args.verbose:
         print(action)
         print(data)
+    (scheme, netloc, path, params, query, fragment) = urlparse.urlparse(action)
+    if '' == scheme:
+        (scheme, netloc, path, params, query, fragment) = urlparse.urlparse(args.site)
+        action = scheme + '://' + netloc + '/' + action
     if postSess(s, action, data, args.loggedInString, args.userAgentString, verbose=args.verbose):
         print('logged in')
     else:
