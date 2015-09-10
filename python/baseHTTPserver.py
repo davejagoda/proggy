f3aef5be1a0b6dc886802b8f7ba75d30edabdc72
Dave Jagoda <davejagoda@gmail.com>
2015-09-10 12:18:32 -0700

    add baseHTTPserver.py

---
+++ python/baseHTTPserver.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# https://www.piware.de/2011/01/creating-an-https-server-in-python/
+# make self-signed cert:
+# openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+
+import BaseHTTPServer, SimpleHTTPServer, ssl, argparse
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', required=True, help='listen port')
+    parser.add_argument('-s', '--ssl', action='store_true', help='SSL')
+    parser.add_argument('-v', '--verbose', action='store_true', help='be verbose')
+    args = parser.parse_args()
+    httpd = BaseHTTPServer.HTTPServer(('',int(args.port)),SimpleHTTPServer.SimpleHTTPRequestHandler)
+    if args.ssl:
+        httpd.socket = ssl.wrap_socket(httpd.socket, certfile='localhost.pem', server_side=True)
+    httpd.serve_forever()
