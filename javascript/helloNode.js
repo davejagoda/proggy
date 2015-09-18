36db722bd0c7dd496c5b7df0044a42da0edfe920
Dave Jagoda <davejagoda@gmail.com>
2015-09-18 10:41:39 -0700

    minor refactor and log request URL and headers

--- javascript/helloNode.js
+++ javascript/helloNode.js
@@ -1,8 +1,13 @@
 var http = require('http');
+var port = 8080
+var data = '<!doctype html>\n<html>\n<head>\n<title>hello</title>\n<meta charset="utf-8" />\n</head>\n<body>\n<b>hello</b>\n</body>\n</html>\n'
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000);
+  console.log('url: ' + req.url);
+  console.log('headers:');
+  console.log(req.headers);
+  res.writeHead(200, {'Content-Type': 'html'});
+  res.end(data);
+}).listen(port);
 
-console.log('Server running at http://127.0.0.1:8000/');
+console.log('server listening on port:' + port);
