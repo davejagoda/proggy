7013283b4a2e5e94d3f4503847cb527422b65ebd
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-09-22 17:22:19 -0700

    add charTool.py

---
+++ python/charTool.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import argparse, codecs
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='file to process')
+    parser.add_argument('-c', '--count', action='store_true', help='count the number of chars')
+    parser.add_argument('-n', '--nth', help='print the nth char')
+    args = parser.parse_args()
+    with codecs.open(args.file, 'r', encoding='utf-8') as f:
+        chars = f.read()
+    if args.count:
+        print(len(chars))
+    if args.nth:
+        print(chars[int(args.nth)])
