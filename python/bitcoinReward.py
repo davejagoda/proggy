f7f242373b03b7f0d2933d348bf3bcddfb383c0a
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-09-24 09:47:44 -0700

    add bitcoinReward.py

---
+++ python/bitcoinReward.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import time
+reward = 50
+sum = 0
+print('               %BTC mined                                 reward')
+print('{:40.36f} {:40.36f}'.format(sum, reward))
+while True:
+    time.sleep(0.5)
+    sum += reward
+    reward = reward / 2.0
+    print('{:40.36f} {:40.36f}'.format(sum, reward))
+    if sum >= 100:
+        print('sum >= 100')
+        break
+    if reward <= 0:
+        print('reward <= 0')
+        break
