ce3460d1ea20c9883df7db3c324ae23868a3a268
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-09-28 09:20:21 -0700

    rewrite freq2wl.py to use argparse

--- python/freq2wl.py
+++ python/freq2wl.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import sys
+import argparse
 
 # speed of light (meters/second)
 C = 299792458.0
 
-# frequency in Hertz (1/second)
-f = float(sys.argv[1])
-
-# wavelength (in meters)
-print C/f
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('frequency', type=float, help='frequency in Hertz (1/second)')
+    args = parser.parse_args()
+    print('wavelength in meters: {}'.format(C/args.frequency))
