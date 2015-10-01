aa65e590be980f2ff925b09ea32513aeb337f5ee
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-10-01 08:45:10 -0700

    add charFreq.py

---
+++ python/charFreq.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import argparse, codecs
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='file to process')
+    args = parser.parse_args()
+    with codecs.open(args.file, 'r', encoding='utf-8') as f:
+        chars = f.read()
+    d = {}
+    for c in chars:
+        if c in d:
+            d[c] += 1
+        else:
+            d[c] = 1
+    for k in sorted(d.keys()):
+        if ord(k) < 32:
+            c = '\\' + chr(96 + ord(k))
+        else:
+            c = k.encode('utf8')
+        print('{}:{}'.format(c, d[k]))
