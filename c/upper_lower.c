8b352fee7bfc5efe6fe11a017a56c5c74ff29f7d
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-10-31 11:38:45 -0700

    add upper_lower.c which is an answer to this question: http://stackoverflow.com/questions/33454894/which-library-in-c-language-can-i-use-for-toupper-and-tolower

---
+++ c/upper_lower.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+  int arg_index, string_index;
+  arg_index = 1;
+  while (arg_index < argc)
+    {
+      printf("original: %s\n", argv[arg_index]);
+      printf("upper: ");
+      for (string_index = 0; string_index < strlen(argv[arg_index]); ++string_index)
+	{
+	  printf("%c", (toupper(argv[arg_index][string_index])));
+	}
+      printf("\n");
+      printf("lower: ");
+      for (string_index = 0; string_index < strlen(argv[arg_index]); ++string_index)
+	{
+	  printf("%c", (tolower(argv[arg_index][string_index])));
+	}
+      printf("\n\n");
+      ++arg_index;
+    }
+}
