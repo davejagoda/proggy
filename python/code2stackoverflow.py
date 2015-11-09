ae6169074d29275c50ac2aa7d10eb948d95e8a66
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-11-09 15:19:07 -0800

    add code2stackoverflow.py

---
+++ python/code2stackoverflow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help='echo the string you use here')
+args = parser.parse_args()
+
+with open(args.file, 'r') as f:
+    for line in f.readlines():
+        print('    {}'.format(line.rstrip()))
