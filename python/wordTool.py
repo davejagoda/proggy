1487460bfa339188fd24d8d4b8137f31d23f0e86
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-11-25 19:08:08 -0800

    modify wordTool.py to print words of a given length

--- python/wordTool.py
+++ python/wordTool.py
@@ -7,6 +7,7 @@ if '__main__' == __name__:
     parser.add_argument('file', help='file to process')
     parser.add_argument('-c', '--count', action='store_true', help='count the number of words')
     parser.add_argument('-n', '--nth', help='print the nth word')
+    parser.add_argument('-l', '--length', type=int, help='print words with the given length')
     args = parser.parse_args()
     with open(args.file, 'r') as f:
         words = f.read().split()
@@ -14,3 +15,7 @@ if '__main__' == __name__:
         print(len(words))
     if args.nth:
         print(words[int(args.nth)])
+    if args.length:
+        for word in words:
+            if len(word) == args.length:
+                print(word)
