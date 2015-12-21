6a64620311b0973c26888de7ea9383843062bad4
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-12-21 09:34:46 -0800

    add min_max.js

---
+++ javascript/min_max.js
@@ -0,0 +1,6 @@
+#!/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc
+
+print(Number.MIN_VALUE);
+print(Number.MAX_VALUE);
+print(Number.MIN_SAFE_INTEGER);
+print(Number.MAX_SAFE_INTEGER);
