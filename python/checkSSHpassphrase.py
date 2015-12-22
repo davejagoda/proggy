6e56160a265ea89f72081e240ba0222a2faab4f9
Dave Jagoda <davejagoda@users.noreply.github.com>
2015-12-22 08:24:53 -0800

    small refactor of checkSSHpassphrase.py

--- python/checkSSHpassphrase.py
+++ python/checkSSHpassphrase.py
@@ -4,9 +4,16 @@ import subprocess, os, getpass
 
 DEVNULL = open(os.devnull, 'wb')
 
-pswd = getpass.getpass()
-try:
-    subprocess.check_call(['ssh-keygen', '-y', '-f', os.getenv('HOME') + '/.ssh/id_rsa', '-P', pswd], stdout=DEVNULL, stderr=DEVNULL)
-    print('that was it!')
-except:
-    print('wrong password')
+def validate_password(pswd):
+    try:
+        subprocess.check_call(['ssh-keygen', '-y', '-f', os.getenv('HOME') + '/.ssh/id_rsa', '-P', pswd], stdout=DEVNULL, stderr=DEVNULL)
+        return(True)
+    except:
+        return(False)
+
+if '__main__' == __name__:
+    pswd = getpass.getpass()
+    if validate_password(pswd):
+        print('that was it!')
+    else:
+        print('wrong password')
