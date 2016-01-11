434dfc8040b7a5f94178cc4f163a6781dda71b19
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-01-11 11:52:52 -0800

    add perl_paragraph_mode.py

---
+++ python/perl_paragraph_mode.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help='file to process')
+parser.add_argument('-v', '--verbose', action='store_true', help='show verbose output')
+args = parser.parse_args()
+if args.verbose: print 'about to begin'
+with open(args.file, 'r') as f:
+    all_data = f.read()
+paragraphs = all_data.split('\n\n')
+print(len(paragraphs))
+if args.verbose: print 'about to end'
