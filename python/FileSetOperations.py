e56506340d8cf1a061c1281476469c4b3029ce1a
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-01-17 06:53:55 -0800

    add difference to FileSetOperations.py

--- python/FileSetOperations.py
+++ python/FileSetOperations.py
@@ -6,11 +6,12 @@ def handleArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('f1', help='first file to check')
     parser.add_argument('f2', help='second file to check')
+    parser.add_argument('-d', '--difference', action='store_true', help='show the difference of the 2 files')
     parser.add_argument('-i', '--intersection', action='store_true', help='show the intersection of the 2 files')
     parser.add_argument('-u', '--union', action='store_true', help='show the union of the 2 files')
     parser.add_argument('-v', '--verbose', action='store_true', help='show the sets generated from the files')
     args = parser.parse_args()
-    return(args.f1, args.f2, args.intersection, args.union, args.verbose)
+    return(args.f1, args.f2, args.difference, args.intersection, args.union, args.verbose)
 
 def fileToSet(file):
     f = open(file)
@@ -19,12 +20,15 @@ def fileToSet(file):
     return(set(lines))
 
 if '__main__' == __name__:
-    (f1, f2, inter, union, verbose) = handleArguments()
+    (f1, f2, diff, inter, union, verbose) = handleArguments()
     s1 = fileToSet(f1)
     s2 = fileToSet(f2)
     if verbose:
         print('s1:'+str(s1))
         print('s2:'+str(s2))
+    if diff:
+        print('s1-s2' + str(s1-s2))
+        print('s2-s1' + str(s2-s1))
     if inter:
         print('intersection:' + str(s1&s2))
     if union:
