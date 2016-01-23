f6ca01344968c8cf5d995da58718f1e96dd8885f
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-01-22 18:42:29 -0800

    in case of an exception, try treating epoch as milliseconds

--- python/iso8601.py
+++ python/iso8601.py
@@ -5,6 +5,9 @@ import datetime, sys
 if 1 == len(sys.argv):
     epoch = datetime.datetime.utcnow()
 else:
-    epoch = datetime.datetime.fromtimestamp(float(sys.argv[1]))
+    try:
+        epoch = datetime.datetime.fromtimestamp(float(sys.argv[1]))
+    except:
+        epoch = datetime.datetime.fromtimestamp(float(sys.argv[1])/1000.0)
 
 print(epoch.replace(microsecond=0).isoformat()+'Z')
