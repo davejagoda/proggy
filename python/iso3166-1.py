6e3cc5fc35d0a508c5a9ed3642e6c5de4005f3f8
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-02-17 15:54:49 -0800

    add iso3166-1.py

---
+++ python/iso3166-1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python -u
+# -*- coding: utf-8 -*-
+
+import argparse, requests, bs4
+
+URL = 'https://en.wikipedia.org/wiki/ISO_3166-1'
+
+def parse_table(soup, verbose=False):
+    results = []
+    table = soup.find('table')
+    if verbose: print(table)
+    row_num = 0
+    for tr in table.find_all('tr'):
+        if verbose: print(tr)
+        td = tr.find_all('td')
+        if 0 == len(td) and 0 == row_num:
+            if verbose: print('header!')
+        else:
+            results.append((td[1].text, td[0].text))
+        row_num += 1
+    return(results)
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='show verbose output')
+    args = parser.parse_args()
+    r = requests.get(URL)
+    if args.verbose: print(r.status_code)
+    assert(200 == r.status_code)
+    soup = bs4.BeautifulSoup(r.content, 'html.parser')
+    if args.verbose: print(soup)
+    broken_bar = unichr(0x00a6)
+    for row in parse_table(soup, args.verbose):
+        print(broken_bar.join(row))
