a3e52064161cacb5786e8d28bd7aa21fc08bf947
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-03-08 10:03:42 -0800

    rewrite argparsy.py to act more like unix mv command

--- python/argparsy.py
+++ python/argparsy.py
@@ -2,12 +2,14 @@
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('echo', help='echo the string you use here')
+parser.add_argument('source', nargs='+', help='the source[s]')
+parser.add_argument('destination', nargs=1, help='the destination directory')
+# mv [-f | -i | -n] [-v] source ... directory
 parser.add_argument('-v', '--verbose', action='store_true', help='show verbose output')
 args = parser.parse_args()
 if args.verbose:
-    print 'about to echo arguments'
-print args.echo
+    print('about to echo arguments')
+print('source[s]:{}'.format(args.source))
+print('destination:{}'.format(args.destination))
 if args.verbose:
-    print 'just echoed arguments'
-
+    print('just echoed arguments')
