a7a4b5256eae02a74cdf2d0fc01d4cccddb96f36
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-03-09 15:44:12 -0800

    enable checking max and min line lengths

--- python/field_counter.py
+++ python/field_counter.py
@@ -5,6 +5,7 @@ import argparse
 if '__main__' == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--delimiter', help='word delimiter')
+    parser.add_argument('-l', '--linelength', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('file', help='file to process')
     args = parser.parse_args()
@@ -12,8 +13,15 @@ if '__main__' == __name__:
         lines = f.readlines()
     max_words = len(lines[0].split(args.delimiter))
     min_words = len(lines[0].split(args.delimiter))
+    max_line = len(lines[0])
+    min_line = len(lines[0])
     for line in lines:
         max_words = max(max_words, len(line.split(args.delimiter)))
         min_words = min(min_words, len(line.split(args.delimiter)))
+        max_line = max(max_line, len(line))
+        min_line = min(min_line, len(line))
     print('max words per line={}'.format(max_words))
     print('min words per line={}'.format(min_words))
+    if args.linelength:
+        print('max length line={}'.format(max_line))
+        print('min length line={}'.format(min_line))
