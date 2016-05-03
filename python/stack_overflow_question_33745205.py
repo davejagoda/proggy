ebae44fe5c767f32c647ac8a91a47f9103e7e204
dj <davejagoda@users.noreply.github.com>
2016-05-02 20:03:38 -0700

    add answer to this question: http://stackoverflow.com/questions/33745205/urllib-request-urlopen-cannot-fetch-the-primaries-page-of-stack-overflow-electio/33746863#33746863

---
+++ python/stack_overflow_question_33745205.py
@@ -0,0 +1,22 @@
+from urllib import request
+
+urls = (
+    'http://math.stackexchange.com/election/5?tab=primary',
+    'http://serverfault.com/election/5?tab=primary',
+    'http://stackoverflow.com/election/7?tab=primary',
+)
+
+for url in urls:
+    print('fetching {} ...'.format(url))
+    try:
+        request.urlopen(url).read()
+    except:
+        print('got an exception, changing user-agent to urllib3 default')
+        req = request.Request(url)
+        req.add_header('User-Agent', 'Python-urllib/3.4')
+        try:
+            request.urlopen(req)
+        except:
+            print('got another exception, changing user-agent to something else')
+            req.add_header('User-Agent', 'not-Python-urllib/3.4')
+            request.urlopen(req)
