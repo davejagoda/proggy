f6c01f23a26cfcb5f374f9ba0dac337053cac8ff
dj <davejagoda@users.noreply.github.com>
2016-05-25 15:20:52 -0700

    Add centrigrade_to_fahrenheit.for

---
+++ fortran/centrigrade_to_fahrenheit.for
@@ -0,0 +1,13 @@
+* columns matter in fortran
+      PROGRAM CENTIGRADE_TO_FAHRENHEIT
+      INTEGER C, Z
+      REAL R
+      PARAMETER (Z = 32)
+      PARAMETER (R = 9.0 / 5.0)
+      DO 10, C = 0, 40
+         F = (C * R) + Z
+         WRITE (*, 99) C, F
+ 99      FORMAT (I2, F6.1)
+ 10   CONTINUE
+      STOP
+      END
