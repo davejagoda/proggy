4b277269d03b3abadee3591abab69c3495cb2619
dj <davejagoda@users.noreply.github.com>
2016-06-23 14:38:48 -0700

    add count_to_10.c count_to_10.java count_to_10.py count_to_10.rb

---
+++ java/count_to_10.java
@@ -0,0 +1,11 @@
+public class count_to_10
+{
+    public static void main(String[] args )
+    {
+        int i;
+        for (i = 0; i < 10; ++i)
+            {
+                System.out.printf("%d\n", i);
+            }
+    }
+}
---
+++ ruby/count_to_10.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+10.times do |i|
+  puts i
+end
