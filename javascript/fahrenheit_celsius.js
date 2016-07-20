40a47e7ee9fdd990cc31e00dd720fe09b4692acd
dj <davejagoda@users.noreply.github.com>
2016-07-20 10:11:19 -0700

    Add fahrenheit_celsius.js

---
+++ javascript/fahrenheit_celsius.js
@@ -0,0 +1,14 @@
+#!/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc
+
+/* columns don't matter in JavaScript */
+
+lower =   0; /* lower limit of temperature table */
+upper = 300; /* upper limit of temperature table */
+step  =  20; /* step size */
+
+fahr = lower;
+while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    print(fahr, celsius.toFixed(1));
+    fahr = fahr + step;
+}
