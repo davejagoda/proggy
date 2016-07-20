bab841eed7db10aae42be76d4a0864bcd6b87c7c
dj <davejagoda@users.noreply.github.com>
2016-07-20 10:38:00 -0700

    Reformat fahrenheit_celsius.java

--- java/fahrenheit_celsius.java
+++ java/fahrenheit_celsius.java
@@ -1,16 +1,12 @@
-/* columns don't matter in Java
- to compile:
- javac fahrenheit_celsius.java */
+/* to compile: javac fahrenheit_celsius.java */
 
 public class fahrenheit_celsius {
     public static void main(String[] args) {
         double fahr, celsius;
         int lower, upper, step;
-
         lower =   0; /* lower limit of temperature table */
         upper = 300; /* upper limit of temperature table */
         step  =  20; /* step size */
-
         fahr = lower;
         while (fahr <= upper) {
             celsius = (5.0/9.0) * (fahr-32.0);
