98655c9eeb1aafb02b1392d16bcec57d792c39de
dj <davejagoda@users.noreply.github.com>
2016-07-20 09:03:39 -0700

    Reformat and rename fahrenheit programs

--- python/fahrenheit-celsius.py
+++ python/fahrenheit-celsius.py
@@ -4,10 +4,10 @@
 
 lower =   0 # lower limit of temperature table
 upper = 300 # upper limit of temperature table
-step =   20 # step size
+step  =  20 # step size
 
 fahr = lower
 while (fahr <= upper):
-    celsius = 5 * (fahr - 32) / 9
-    print('{}\t{}'.format(fahr, celsius))
+    celsius = (5.0/9.0) * (fahr-32.0)
+    print('{:3} {:6.1f}'.format(fahr, celsius))
     fahr = fahr + step
--- ruby/fahrenheit-celsius.rb
+++ ruby/fahrenheit-celsius.rb
@@ -4,11 +4,11 @@
 
 lower =   0 # lower limit of temperature table
 upper = 300 # upper limit of temperature table
-step =   20 # step size
+step  =  20 # step size
 
 fahr = lower
 while (fahr <= upper)
-    celsius = 5 * (fahr - 32) / 9
-    printf("%d\t%d\n", fahr, celsius);
+    celsius = (5.0/9.0) * (fahr-32.0)
+    printf("%3.0f %6.1f\n", fahr, celsius)
     fahr = fahr + step
 end
