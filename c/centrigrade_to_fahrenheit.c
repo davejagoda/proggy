fb66119fa712d10d6ec68162492fcc076c30f149
dj <davejagoda@users.noreply.github.com>
2016-07-19 18:17:53 -0700

    Add centrigrade_to_fahrenheit.c

---
+++ c/centrigrade_to_fahrenheit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* columns don't matter in c
+ to compile:
+ gcc centrigrade_to_fahrenheit.c -o centrigrade_to_fahrenheit */
+
+int main()
+{
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;   /* lower limit of temperature table */
+  upper = 300; /* upper limit of temperature table */
+  step = 20;   /* step size */
+
+  fahr = lower;
+  while (fahr <= upper ) {
+    celsius = 5 * (fahr - 32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
