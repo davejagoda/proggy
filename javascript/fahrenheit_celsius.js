8330ddb1b5f9fa5e831598d218192b5d0c3c34e5
dj <davejagoda@users.noreply.github.com>
2016-07-20 18:24:22 -0700

    Rename leftPad to pad in fahrenheit_celsius.js

--- javascript/fahrenheit_celsius.js
+++ javascript/fahrenheit_celsius.js
@@ -1,17 +1,15 @@
 #!/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc
 
-function leftPad (str, len) {
+function pad (str, len) {
     while (str.length < len) str = ' ' + str;
     return str;
 }
-
 var lower =   0; /* lower limit of temperature table */
 var upper = 300; /* upper limit of temperature table */
 var step  =  20; /* step size */
-
 var fahr = lower;
 while (fahr <= upper) {
     var celsius = (5.0/9.0) * (fahr-32.0);
-    print(leftPad(fahr.toFixed(0), 3), leftPad(celsius.toFixed(1), 6));
+    print(pad(fahr.toFixed(0), 3), pad(celsius.toFixed(1), 6));
     fahr = fahr + step;
 }
