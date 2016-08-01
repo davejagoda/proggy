7ed5c920fd9c62472c29b2d54928ba0c115717ba
dj <davejagoda@users.noreply.github.com>
2016-08-01 16:13:59 -0700

    Modify apache_vhost_generator.py for python2.6 and quote some values

--- python/apache_vhost_generator.py
+++ python/apache_vhost_generator.py
@@ -3,28 +3,26 @@
 import argparse
 import os
 
-vhost_template = '''
-<VirtualHost *:{1}>
+vhost_template = '''<VirtualHost *:{1}>
     ServerName {0}
-    DocumentRoot /var/www/vhosts/{0}
-    <Directory /var/www/vhosts/{0}>
+    DocumentRoot "/var/www/vhosts/{0}"
+    <Directory "/var/www/vhosts/{0}">
         Options Indexes FollowSymLinks MultiViews
         AllowOverride All
     </Directory>
-    CustomLog /var/log/httpd/{0}-access.log combined
-    ErrorLog /var/log/httpd/{0}-error.log
-# one of debug, info, notice, warn, error, crit, alert, emerg
+    CustomLog "/var/log/httpd/{0}-access.log" combined
+    ErrorLog "/var/log/httpd/{0}-error.log"
     LogLevel warn
 </VirtualHost>
 '''
 
 def write_vhost_conf(server_name, port, destination_directory):
-    full_path = '{}/{}.conf'.format(destination_directory, server_name)
+    full_path = '{0}/{1}.conf'.format(destination_directory, server_name)
     if os.path.isfile(full_path):
-        return('file {} exists, not overwriting'.format(full_path))
+        return('file {0} exists, not overwriting'.format(full_path))
     with open(full_path, 'w') as f:
         f.write(vhost_template.format(server_name, port))
-        return('file {} created'.format(full_path))
+        return('file {0} created'.format(full_path))
     return('something unexpected happened')
 
 if '__main__' == __name__:
