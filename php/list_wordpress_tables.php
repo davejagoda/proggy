9dd40c7fc3bd36877e13adee79f214a7c6562fb3
dj <davejagoda@users.noreply.github.com>
2016-08-02 12:01:52 -0700

    Add list_wordpress_tables.php

---
+++ php/list_wordpress_tables.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+if (2 != $argc) {
+    echo "need exactly one argument: full path to wp-config.php file";
+    exit;
+}
+
+require_once $argv[1];
+
+if (! mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)) {
+    echo "MySQL error:" . mysql_error();
+    exit;
+}
+
+$tables = mysql_list_tables(DB_NAME);
+while($table = mysql_fetch_row($tables)) {
+    echo "$table[0]\n";
+}
