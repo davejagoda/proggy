f7729ae217859d3e3215470c3f93eae98fc70f05
dj <davejagoda@users.noreply.github.com>
2016-08-23 14:24:11 -0700

    Update requestBeauty.py to use html5lib parser

--- python/requestBeauty.py
+++ python/requestBeauty.py
@@ -5,7 +5,7 @@
 # ./requestBeauty.py -s https://stormy-escarpment-6340.herokuapp.com/admin -u <USERNAME> -p <PASSWORD> -l 'Welcome'
 import argparse, requests, bs4, re, urlparse
 
-def getSess(url, username, password, useragent, verbose=False):
+def getSess(url, username, password, useragent, verbose=0):
     if useragent:
         headers = {'User-Agent': useragent}
     else:
@@ -13,24 +13,26 @@ def getSess(url, username, password, useragent, verbose=False):
     data=[]
     s = requests.Session()
     r = s.get(url, headers=headers)
-    if verbose:
-        print(r.status_code)
-        print(r.headers)
+    if verbose > 0:
+        print('HTTP status_code:{}'.format(r.status_code))
+        print('HTTP history:{}'.format(r.history))
+        print('HTTP headers:{}'.format(r.headers))
+    if verbose > 1:
         print(r.content)
-    soup = bs4.BeautifulSoup(r.content, "html.parser")
-    if verbose:
+    soup = bs4.BeautifulSoup(r.content, 'html5lib')
+    if verbose > 1:
         print(soup)
     for f in soup.find_all('form'):
         if 'post' == f['method']:
             action = f['action']
-            if verbose:
+            if verbose > 0:
                 print('ACTION:',action)
             for c in f.find_all('input'):
                 try:
                     value = c['value']
                 except:
                     value = ''
-                if verbose:
+                if verbose > 0:
                     try:
                         print(c['name'],c['type'], value.encode('utf8'))
                     except:
@@ -45,22 +47,24 @@ def getSess(url, username, password, useragent, verbose=False):
                     print('no name exception:{}'.format(c))
     return(s, action, data)
 
-def postSess(s, url, data, loggedInString, useragent, verbose=False):
+def postSess(s, url, data, loggedInString, useragent, verbose=0):
     if useragent:
         headers = {'User-Agent': useragent}
     else:
         headers = {'User-Agent': 'Mozilla/5.0'}
     headers['Referer'] = url
-    if verbose:
+    if verbose > 0:
         print('url:{}'.format(url))
         print('headers:{}'.format(headers))
         print('data:{}'.format(data))
     r = s.post(url, headers=headers, data=data)
-    if verbose:
-        print(r.status_code)
-        print(r.headers)
+    if verbose > 0:
+        print('HTTP status_code:{}'.format(r.status_code))
+        print('HTTP history:{}'.format(r.history))
+        print('HTTP headers:{}'.format(r.headers))
+    if verbose > 1:
         print(r.content)
-    soup = bs4.BeautifulSoup(r.content, "html.parser")
+    soup = bs4.BeautifulSoup(r.content, 'html5lib')
     return(soup.find(text=re.compile(loggedInString)) is not None)
 
 if '__main__' == __name__:
@@ -70,12 +74,12 @@ if '__main__' == __name__:
     parser.add_argument('-p', '--password', required=True)
     parser.add_argument('-l', '--loggedInString', required=True)
     parser.add_argument('-a', '--userAgentString')
-    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-v', '--verbose', action='count')
     args = parser.parse_args()
     if args.verbose:
         print(args.site, args.username, args.password)
     (s, action, data) = getSess(args.site, args.username, args.password, args.userAgentString, verbose=args.verbose)
-    if args.verbose:
+    if args.verbose > 0:
         print(action)
         print(data)
     (scheme, netloc, path, params, query, fragment) = urlparse.urlparse(action)
