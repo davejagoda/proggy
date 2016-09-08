9fccdbbd9897b9e864a768846bf1a8cc93c45139
dj <davejagoda@users.noreply.github.com>
2016-09-08 16:29:43 -0700

    Add --timeout and --verbose options to request_count_tags_casper.js

--- javascript/request_count_tags_casper.js
+++ javascript/request_count_tags_casper.js
@@ -3,16 +3,34 @@ var casper = require('casper').create();
 if (casper.cli.args.length === 2) {
     var url = casper.cli.args[0];
     var tag = casper.cli.args[1];
+    if (casper.cli.has('timeout')) {
+        var timeout = casper.cli.get('timeout');
+    } else {
+        var timeout = 0;
+    }
+    if (casper.cli.has('verbose')) {
+        var verbose = true;
+    } else {
+        var verbose = false;
+    }
 } else {
-    casper.echo('Need exactly two arguments: url and tag').exit();
+    casper.echo('Need exactly two arguments: url and tag');
+    casper.echo('Option: --timeout=#seconds');
+    casper.echo('Option: --verbose');
+    casper.exit(1);
 }
 
 casper.start(url, function() {
-    var tags = [];
-    if (this.exists(tag)) {
-        tags = this.getElementsInfo(tag);
+    if (verbose) {
+        this.echo('url=' + url + ' tag=' + tag + ' timeout=' + timeout);
     }
-    this.echo('number of tags of type:' + tag + ' is ' + tags.length);
+    this.wait(timeout * 1000, function() {
+        var tags = [];
+        if (this.exists(tag)) {
+            tags = this.getElementsInfo(tag);
+        }
+        this.echo('number of tags of type:' + tag + ' is ' + tags.length);
+    });
 });
 
 casper.run();
