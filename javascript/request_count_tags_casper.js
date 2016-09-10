98e96506733005416e07c5c0f4cff050bd037658
dj <davejagoda@users.noreply.github.com>
2016-09-10 07:12:44 -0700

    Add selector to request_count_tags_casper.js

--- javascript/request_count_tags_casper.js
+++ javascript/request_count_tags_casper.js
@@ -3,6 +3,11 @@ var casper = require('casper').create();
 if (casper.cli.args.length === 2) {
     var url = casper.cli.args[0];
     var tag = casper.cli.args[1];
+    if (casper.cli.has('selector')) {
+        var selector = casper.cli.get('selector');
+    } else {
+        var selector = '';
+    }
     if (casper.cli.has('timeout')) {
         var timeout = casper.cli.get('timeout');
     } else {
@@ -15,15 +20,24 @@ if (casper.cli.args.length === 2) {
     }
 } else {
     casper.echo('Need exactly two arguments: url and tag');
-    casper.echo('Option: --timeout=#seconds');
+    casper.echo('Option: --selector=#id or .class');
+    casper.echo('Option: --timeout=seconds');
     casper.echo('Option: --verbose');
+    casper.echo('Tip: if you want to count just the tags that match the query selector');
+    casper.echo('     supply tag as tag.class or tag#id');
+    casper.echo('     since it waits for tag+selector and counts tag');
     casper.exit(1);
 }
 
 casper.start(url, function() {
     if (verbose) {
-        this.echo('url=' + url + ' tag=' + tag + ' timeout=' + timeout);
+        this.echo('url=' + url + ' tag=' + tag + ' selector=' + selector + ' timeout=' + timeout);
     }
+    // wait until a tag with the selector is present
+    this.waitForSelector(tag + selector, function() {
+        this.echo('found: ' + tag + selector);
+    });
+    // wait timeout seconds for the page to load
     this.wait(timeout * 1000, function() {
         var tags = [];
         if (this.exists(tag)) {
