be94b14c0958b056ae4dfeaa46fa89d9f102ff1e
dj <davejagoda@users.noreply.github.com>
2016-09-14 17:28:55 -0700

    Add uniq_words.js

---
+++ javascript/uniq_words.js
@@ -0,0 +1,20 @@
+fs = require('fs');
+
+if (3 !== process.argv.length) {
+    console.log('Provide exactly one argument: a file to read');
+    process.exit(1);
+}
+
+var file = process.argv[2];
+var uniq = {};
+
+fs.readFile(file, 'utf8', function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    data.split('\n').forEach(function(entry) {
+        console.log(entry);
+        uniq[entry] = uniq[entry] + 1 || 1;
+    });
+    console.log(uniq);
+});
