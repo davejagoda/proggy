6a33392f83d85f46e62fb3256bdaee0629db9101
dj <davejagoda@users.noreply.github.com>
2017-01-13 17:55:35 -0800

    Add japanese.py and remove Hiragana.py and Katakana.py

---
+++ python/japanese.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import unicodedata
+
+def make_table_of_ranges():
+    ranges = {}
+    ranges['hiragana'] = (0x3040, 0x309f)
+    ranges['katakana'] = (0x30a0, 0x30ff)
+    return(ranges)
+
+def print_range(lo, hi):
+    for i in range(lo, hi+1):
+        c = chr(i)
+        try:
+            name = unicodedata.name(c)
+        except:
+            name = 'NONE'
+        if '' == unicodedata.decomposition(c) and 'LETTER' in name and 'SMALL' not in name:
+            print(c)
+#        print(c.encode('utf8'), name)
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    ranges = make_table_of_ranges()
+    for r in ranges.keys():
+        arg_name = '--{}'.format(r)
+        parser.add_argument(arg_name, action='store_true')
+    args = parser.parse_args()
+    for k, v in args.__dict__.items():
+        if v:
+            print(k)
+            print_range(ranges[k][0], ranges[k][1])
