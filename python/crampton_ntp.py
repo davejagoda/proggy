cc8d1b8cc825afb145c4dd535bed7c66ee66edfc
dj <davejagoda@users.noreply.github.com>
2017-03-16 14:07:41 -0700

    Add crampton_ntp.py

---
+++ python/crampton_ntp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# http://blog.mattcrampton.com/post/88291892461/query-an-ntp-server-from-python
+
+from socket import AF_INET, SOCK_DGRAM
+import sys
+import socket
+import struct, time, datetime
+
+def iso8601(epoch):
+    return datetime.datetime.fromtimestamp(epoch).isoformat()+'Z'
+
+def getMyTime():
+    return time.time()
+
+def getNTPTime(host = "pool.ntp.org"):
+    port = 123
+    buf = 1024
+    address = (host,port)
+    msg = '\x1b' + 47 * '\0'
+
+    # reference time (in seconds since 1900-01-01 00:00:00)
+    TIME1970 = 2208988800L # 1970-01-01 00:00:00
+
+    # connect to server
+    client = socket.socket( AF_INET, SOCK_DGRAM)
+    client.sendto(msg, address)
+    msg, address = client.recvfrom( buf )
+
+    t = struct.unpack( "!12I", msg )[10] + \
+        float(struct.unpack( "!12I", msg )[11]) / 2**32
+    t -= TIME1970
+    return t
+
+if __name__ == "__main__":
+    times = []
+    times.append(['loc', getMyTime()])
+    times.append(['ntp', getNTPTime()])
+    times.append(['loc', getMyTime()])
+    t_prev = times[0][1]
+    for d,t in times:
+        print('{}:{}'.format(d,iso8601(t)))
+#        print(t - t_prev)
+        assert(t - t_prev >= 0)
+        t_prev = t
