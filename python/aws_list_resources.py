bfc74f831b423eeee4239703a472f4380de17264
dj <davejagoda@users.noreply.github.com>
2017-04-19 17:47:18 -0700

    Add threading to aws_list_resources.py

--- python/aws_list_resources.py
+++ python/aws_list_resources.py
@@ -1,28 +1,57 @@
 #!/usr/bin/env python
 
+# http://stackoverflow.com/questions/2846653/how-to-use-threading-in-python
+
+import argparse
 import boto3
 import pprint
+import Queue
+import threading
 
-def get_instance_data_from_region(region_name):
-    ec2 = boto3.client('ec2', region_name=region_name)
+def output_region_data(element):
+    (region_name, resources) = element
+    if 0 == len(resources):
+        print('{}:{}'.format(region_name, 'None'))
+    else:
+        print(region_name)
+        for item in resources:
+            pprint.pprint(item)
+
+def get_instance_data_from_region(q, region_name):
+    session = boto3.session.Session(region_name=region_name)
+    ec2 = session.client('ec2')
     response = ec2.describe_instances()
     assert(2 == len(response))
     for resource in response:
         if 'ResponseMetadata' != resource:
             assert('Reservations' == resource)
-            if 0 == len(response[resource]):
-                print('{}:{}'.format(region_name, 'None'))
-            else:
-                print(region_name)
-                assert(list == type(response[resource]))
-                for item in response[resource]:
-                    pprint.pprint(item)
+            assert(list == type(response[resource]))
+            q.put((region_name, response[resource]))
+    return
 
 if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--threads', action='store_true',
+                        help='use this argument to run mutiple threads')
+    args = parser.parse_args()
     ec2 = boto3.client('ec2')
     response = ec2.describe_regions()
+    q = Queue.Queue()
+    thread_list = []
     for region in response['Regions']:
         assert(2 == len(region))
         region_name = region['RegionName']
-        assert('ec2.{}.amazonaws.com'.format(region_name) == region['Endpoint'])
-        get_instance_data_from_region(region_name)
+        assert('ec2.{}.amazonaws.com'.format(region_name) ==
+               region['Endpoint'])
+        if args.threads:
+            thread_list.append(threading.Thread(target=
+                                                get_instance_data_from_region,
+                                                args=(q, region_name)))
+        else:
+            get_instance_data_from_region(q, region_name)
+    for t in thread_list:
+        t.start()
+    for t in thread_list:
+        t.join()
+    while not q.empty():
+        output_region_data(q.get())
