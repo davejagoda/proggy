90b488e805f20d335ee369623b8d4b738474bfb9
dj <davejagoda@users.noreply.github.com>
2017-05-06 09:15:04 -0700

    Add simple_thread.py

---
+++ python/simple_thread.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# https://docs.python.org/2/library/queue.html
+
+import argparse
+import Queue
+import threading
+
+def source():
+    with open('/usr/share/dict/words', 'r') as f:
+        return([line.rstrip() for line in f.readlines()])
+
+def do_work(item):
+    print '{}:{}\n'.format(item, len(item)),
+
+def worker():
+    while True:
+        item = q.get()
+        do_work(item)
+        q.task_done()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--thread_count', type=int, default=1)
+parser.add_argument('-v', '--verbose')
+args = parser.parse_args()
+if args.verbose:
+    print('thread_count:{}'.format(args.thread_count))
+
+q = Queue.Queue()
+for i in range(args.thread_count):
+    t = threading.Thread(target=worker)
+    t.daemon = True
+    t.start()
+
+for item in source():
+    if args.verbose:
+        print('raw item:{}'.format(item))
+    q.put(item)
+
+q.join() # block until all tasks are done
