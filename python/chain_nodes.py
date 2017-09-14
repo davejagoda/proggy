8af86929b8597a412fb8b0f07d7b2b1c70bb49b2
dj <davejagoda@users.noreply.github.com>
2017-09-14 18:13:00 +0000

    Document chain_nodes.py

--- python/chain_nodes.py
+++ python/chain_nodes.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# this program expects a file containing a list of nodes, one per line
+# it will create a chain of nodes, randomly choosing from the remaining nodes
+# then printing them out as colon separated pairs
+
 import argparse
 import random
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument('file')
-    parser.add_argument('-v', '--verbose', action='store_true', help='show verbose output')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='show verbose output')
     args = parser.parse_args()
     with(open(args.file, 'r')) as f:
          nodes = f.read().splitlines()
