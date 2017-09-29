7acec24411fbf875458b1611860b551f17f626e5
dj <davejagoda@users.noreply.github.com>
2017-09-29 04:37:02 +0000

    Add django_count_all_objects_in_all_models.py

---
+++ python/django_count_all_objects_in_all_models.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+you will need to do all (or most) of the following to get this to work:
+source venv/bin/activate
+export DJANGO_SETTINGS_MODULE=<APPNAME>.settings
+export PGUSER=<USERNAME>
+export PGPASSWORD=<PASSWORD>
+export PGHOST=<HOSTNAME>
+'''
+import django
+django.setup()
+
+for m in django.apps.apps.get_models():
+    print('{}:{}'.format(m.__name__,m.objects.count()))
