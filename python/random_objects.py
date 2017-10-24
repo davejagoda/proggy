b894f6bba91e31032f0a558dde852007577c34e2
dj <davejagoda@users.noreply.github.com>
2017-10-24 15:58:23 -0700

    Add random_objects.py

---
+++ python/random_objects.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import random
+
+class RandomClass:
+    """A class with random objects"""
+    def __init__(self, name, rank):
+        self.name = name
+        self.rank = rank
+    def __repr__(self):
+        return('name:{} rank:{}'.format(self.name, self.rank))
+
+def random_generator():
+    ranks = range(10)
+    objects = []
+    while ranks:
+        selected = ranks.pop(random.randrange(len(ranks)))
+        objects.append(RandomClass(chr(65+selected), selected))
+    while objects:
+        yield(objects.pop(random.randrange(len(objects))))
+
+if __name__ == '__main__':
+    min_id = 0
+    for o in sorted([o for o in random_generator()]):
+        print('id:{} {}'.format(id(o), o))
+        assert(min_id < id(o))
+        min_id = id(o)
