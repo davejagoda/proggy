d82257e65aff32e389faafb82a124f04b4414e78
dj <davejagoda@users.noreply.github.com>
2018-02-23 11:44:40 -0800

    Add aws_list_dns.py

---
+++ python/aws_list_dns.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import argparse
+import boto3
+import pprint
+
+def print_domains(response, verbose):
+    print('DOMAINS')
+    if verbose > 0:
+        print('Number of domains found:{}'.format(len(response['Domains'])))
+        pprint.pprint(response)
+    for domain in response['Domains']:
+        print(domain['DomainName'])
+
+def print_zones(response, verbose):
+    print('ZONES')
+    if verbose > 0:
+        print('Number of zones found:{}'.format(len(response['HostedZones'])))
+        pprint.pprint(response)
+    for zone in response['HostedZones']:
+        print(zone['Name'])
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    parser.add_argument('-z', '--zone')
+    args = parser.parse_args()
+    response = boto3.client('route53domains')
+    print_domains(response.list_domains(), args.verbose)
+    response = boto3.client('route53')
+    print_zones(response.list_hosted_zones(), args.verbose)
