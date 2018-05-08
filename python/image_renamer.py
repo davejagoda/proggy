0192625b12ed7ef8d370acc269915ccb46249b50
dj <davejagoda@users.noreply.github.com>
2018-05-08 12:32:11 -0700

    Add subsec to filename

--- python/image_renamer.py
+++ python/image_renamer.py
@@ -4,6 +4,7 @@ import argparse
 import PIL.Image
 import PIL.ExifTags
 import os
+import pprint
 import subprocess
 
 def openImageFile(filename):
@@ -12,16 +13,19 @@ def openImageFile(filename):
     except:
         subprocess.call(['open', filename])
 
-def generateNameFromExif(filename):
+def generateNameFromExif(filename, verbose):
 # https://stackoverflow.com/questions/4764932/in-python-how-do-i-read-the-exif-data-for-an-image#4765242
     exif = {
         PIL.ExifTags.TAGS[k]: v
         for k, v in PIL.Image.open(filename)._getexif().items()
         if k in PIL.ExifTags.TAGS
     }
-    (date_str, time_str) = exif['DateTime'].replace(':','').split()
+    if verbose > 1:
+        pprint.pprint(exif)
+    (date_str, time_str) = exif['DateTime'].replace(':','-').split()
+    subsec =  exif['SubsecTimeOriginal']
     camera = exif['Model'].replace(' ','_')
-    return('{}T{}00{}.jpg'.format(date_str, time_str, camera))
+    return('{}T{}.{}{}.jpg'.format(date_str, time_str, subsec, camera))
 
 def askToRename(oldName, newName, homedir):
     newPath = '{}/Documents/efoto/{}'.format(homedir, newName)
@@ -39,10 +43,17 @@ if '__main__' == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filename', required=True,
                         help='the file to be processed')
+    parser.add_argument('-r', '--rename', action='store_true',
+                        help='the file to be processed')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='show verbose output')
     args = parser.parse_args()
     if os.path.isfile(args.filename):
-        newName = generateNameFromExif(args.filename)
+        newName = generateNameFromExif(args.filename, args.verbose)
+        if args.verbose > 0:
+            print(newName)
         openImageFile(args.filename)
-        askToRename(args.filename, newName, homedir)
+        if args.rename:
+            askToRename(args.filename, newName, homedir)
     else:
         print('{} not found'.format(args.filename))
