e3e0caaf6f7fa45de8f87f21e777caa495558652
dj <davejagoda@users.noreply.github.com>
2018-05-11 00:10:36 +0000

    Fix encoding error in 一.py introduced by upgrade to python3

--- python/一.py
+++ python/一.py
@@ -5,7 +5,7 @@ import unicodedata
 def print_unicode_entry(n):
     u = chr(n)
     print('{:8d} {:8x}'.format(n, n), end=' ')
-    print(u.encode('utf8'), unicodedata.category(u), end=' ')
+    print(u, unicodedata.category(u), end=' ')
     try:
         print(unicodedata.name(u), end=' ')
     except:
