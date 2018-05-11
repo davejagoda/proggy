acdd7fd9da445e4d3000cadb0502a61b3c5e0b15
dj <davejagoda@users.noreply.github.com>
2018-05-12 03:04:19 +0545

    Fix getTimeZoneOffSet.py so it works for offsets on fractions of an hour, moved to python3

--- python/getTimeZoneOffSet.py
+++ python/getTimeZoneOffSet.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+SECONDS_PER_MINUTE = 60
+MINUTES_PER_HOUR = 60
+SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR
 
 import time
 
@@ -6,11 +10,22 @@ if time.localtime().tm_isdst:
     offset = time.altzone
 else:
     offset = time.timezone
-hh = offset / -3600
-mm = offset % 60
-if hh < 0:
+
+if offset > 0:
     sign = '-'
-    hh *= -1
 else:
     sign = '+'
+
+# more obvious 'floor' division
+hh = abs(int(offset / SECONDS_PER_HOUR))
+mm = (abs(offset) - (hh * SECONDS_PER_HOUR)) // SECONDS_PER_MINUTE
+
 print('{}{:02d}:{:02d}'.format(sign,hh,mm))
+
+'''
+To test:
+cd /usr/share/zoneinfo
+for i in `find . -print | cut -b 3-`; do echo -n "$i "; TZ=$i /bin/date "+%z"; done > /tmp/sh
+for i in `find . -print | cut -b 3-`; do echo -n "$i "; TZ=$i ~/src/github/proggy/python/getTimeZoneOffSet.py | tr -d : ; done > /tmp/py
+diff /tmp/sh /tmp/py
+'''
