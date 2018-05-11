2d3f1fd7bff73a7e4b6104897092f6a3e1694325
dj <davejagoda@users.noreply.github.com>
2018-05-10 17:07:23 -0700

    Fix TalkingThatForeign.py breakage due to python3 upgrade

--- python/TalkingThatForeign.py
+++ python/TalkingThatForeign.py
@@ -2,14 +2,14 @@
 
 import subprocess, sys, re
 
-stdout = subprocess.check_output(['say', '-v', '?'])
+stdout = subprocess.check_output(['say', '-v', '?'], universal_newlines=True)
 
 if (2 == len(sys.argv)):
     pattern = sys.argv[1]
 else:
     pattern = 'en_US'
 
-for line in stdout.split('\n'):
+for line in stdout.splitlines():
     if len(line) > 0:
         (left,right) = line.split('#')
         if (None == re.search(pattern, left)):
