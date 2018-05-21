ed1d752cd3c964fb80a46563fbb17a3edad406d2
dj <davejagoda@users.noreply.github.com>
2018-05-21 18:29:29 +0000

    Convert findCR.py to python3

--- python/findCR.py
+++ python/findCR.py
@@ -1,15 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import sys, codecs, fileinput, string, unicodedata
+import codecs
+import fileinput
+import string
+import sys
 
 def process_line(line):
-    if '\r' in line:
-        print('file:{0} line:{1}'.format(fileinput.filename(), fileinput.filelineno()))
+    if '\r'.encode() in line:
+        print('file:{0} line:{1}'.format(fileinput.filename(),
+                                         fileinput.filelineno()))
 
 if '__main__' == __name__:
     current_file = None
     sys.stdin = codecs.getreader('utf-8')(sys.stdin)
-    for line in fileinput.input(openhook=fileinput.hook_encoded('utf-8')):
+    for line in fileinput.input(mode='rb'):
         if fileinput.filename() != current_file:
             current_file = fileinput.filename()
             print('processing:{0}'.format(current_file))
