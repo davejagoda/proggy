647d03d8973a8cedda00d7dfd4d743c1d404a666
dj <davejagoda@users.noreply.github.com>
2018-06-04 18:15:32 +0000

    Convert nancy.py to python3

--- python/nancy.py
+++ python/nancy.py
@@ -1,8 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # converts SIDs (Nancy Spungen reference)
 # very useful resource:
 # http://www.selfadsi.org/deep-inside/microsoft-sid-attributes.htm
+# another useful resource:
+# https://serverfault.com/a/852338
+# sample invocation:
+# ./nancy.py AQUAAAAAAAUVAAAAPWW1S5rojK4mDAiG5BAAAA==
+# expected result:
+# S-1-5-21-1270179133-2928470170-2248674342-4324
 
 import argparse, base64, struct
 
@@ -10,13 +16,15 @@ def convertFromSIDString(s):
     return(base64.b64decode(s))
 
 def unpackBinarySID(binarySIDString):
-    revision, subIDcount, idAuthority, subIDs = (struct.unpack('>bb6s20s',binarySIDString))
+    revision, subIDcount, idAuthority, subIDs = (
+        struct.unpack('>bb6s20s',binarySIDString)
+    )
     assert 5 == subIDcount
     first2bytes, last4bytes = (struct.unpack('>HI', idAuthority))
     assert 0 == first2bytes
     idAuthority = last4bytes
     listOfIDs = [revision, idAuthority] + list(struct.unpack('<IIIII',subIDs))
-    listOfStrings = ['S'] + map(str, listOfIDs)
+    listOfStrings = ['S'] + list(map(str, listOfIDs))
     SID = '-'.join(listOfStrings)
     return(SID)
 
