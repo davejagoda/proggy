a806733735a9eaf7f91879ce909652d4c8c6d8d0
dj <davejagoda@users.noreply.github.com>
2018-08-16 02:21:52 +0000

    Use (new Date()).toISOString() in epoch.js

--- javascript/epoch.js
+++ javascript/epoch.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
 function main() {
-  var zero_epoch = new Date(0);
   var curr_epoch = new Date();
-  console.log('0000000000'                + ' ' + zero_epoch.toISOString());
+  console.log('0000000000'                + ' ' + (new Date(0)).toISOString());
   console.log(Math.floor(curr_epoch/1000) + ' ' + curr_epoch.toISOString());
 }
 
