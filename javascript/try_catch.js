002df323990309f7666427c18ff7604621def699
dj <davejagoda@users.noreply.github.com>
2018-08-16 14:15:19 +0000

    Add try_catch.js

---
+++ javascript/try_catch.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var good_json = '[]';
+var bad_json = '<!DOCTYPE HTML>';
+
+function main() {
+  try {
+    JSON.parse(good_json);
+    console.log('parsed good_json');
+    JSON.parse(bad_json);
+    console.log('parsed bad_json');
+  }
+  catch (error) {
+    console.log('got an exception while parsing JSON');
+  }
+}
+
+main();
+console.log('returned from main()');
