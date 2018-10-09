fb544302dcb0a360122d49059ab44abc9bb0e16e
dj <davejagoda@users.noreply.github.com>
2018-10-09 13:05:43 -0700

    Modify prettyJSON.py to use Python3.4+ defaults

--- python/prettyJSON.py
+++ python/prettyJSON.py
@@ -5,5 +5,6 @@ import sys, json
 for file in sys.argv[1:]:
     with open(file, 'r') as f:
         print('file: {}'.format(file))
-        print(json.dumps(json.loads(f.read()), indent=2, sort_keys=True))
+        print(json.dumps(json.loads(f.read()), indent=2, sort_keys=True,
+                         separators=(',', ': ')))
         print()
