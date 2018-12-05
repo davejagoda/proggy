a0e897c54422a0906f536cc1c8cb79d16ff34bf4
dj <davejagoda@users.noreply.github.com>
2018-12-05 15:47:22 -0800

    Modify import-module.js to use let instead of var

--- javascript/import-module.js
+++ javascript/import-module.js
@@ -2,7 +2,7 @@
 
 console.log('begin');
 const {square, cube} = require('./module');
-for (var i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {
   console.log(i, square(i), cube(i));
 }
 console.log('end');
