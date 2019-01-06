3a4f2e251a1b0fe6349be113da0c15e72c6acd92
dj <davejagoda+github@gmail.com>
2019-01-05 19:37:40 -0800

    Add Volumes and NetworkInterfaces to aws_list_ec2_resources.py

--- python/aws_list_ec2_resources.py
+++ python/aws_list_ec2_resources.py
@@ -2,6 +2,9 @@
 
 # http://stackoverflow.com/questions/2846653/how-to-use-threading-in-python
 
+# TBD
+# Termination Protection
+
 import argparse
 import boto3
 import queue
@@ -9,17 +12,19 @@ import threading
 import aws_lib
 
 def output_region_data(element):
-    (region_name, keys, resv, secg) = element
+    (region_name, keyp, resv, vols, neti, secg) = element
     print(region_name)
-    if keys: print(keys)
+    if keyp: print(keyp)
     if resv: print(resv)
+    if vols: print(vols)
+    if neti: print(neti)
     if secg: print(secg)
 
-def parse_keys_response(response):
-    keys = aws_lib.extract_response(response, 'KeyPairs')
+def parse_keyp_response(response):
+    keyp = aws_lib.extract_response(response, 'KeyPairs')
     return('\n'.join(['K {} {}'.format(
         k['KeyFingerprint'],
-        k['KeyName']) for k in keys]))
+        k['KeyName']) for k in keyp]))
 
 def parse_resv_response(response):
     resv = aws_lib.extract_response(response, 'Reservations')
@@ -29,6 +34,19 @@ def parse_resv_response(response):
         r['Instances'][0]['InstanceType'],
         r['Instances'][0]['PublicDnsName'],
         r['Instances'][0]['KeyName']) for r in resv]))
+
+def parse_vols_response(response):
+    vols = aws_lib.extract_response(response, 'Volumes')
+    return('\n'.join(['V {} {}GB {}'.format(
+        v['VolumeId'],
+        v['Size'],
+        v['VolumeType']) for v in vols]))
+
+def parse_neti_response(response):
+    neti = aws_lib.extract_response(response, 'NetworkInterfaces')
+    return('\n'.join(['N {} {}'.format(
+        n['NetworkInterfaceId'],
+        n['Association']['PublicDnsName']) for n in neti]))
 
 def parse_secg_response(response):
     secg = aws_lib.extract_response(response, 'SecurityGroups')
@@ -41,11 +59,13 @@ def get_instance_data_from_region(q, region_name):
     session = boto3.session.Session(region_name=region_name)
     ec2 = session.client('ec2')
 
-    keys = parse_keys_response(ec2.describe_key_pairs())
+    keyp = parse_keyp_response(ec2.describe_key_pairs())
     resv = parse_resv_response(ec2.describe_instances())
+    vols = parse_vols_response(ec2.describe_volumes())
+    neti = parse_neti_response(ec2.describe_network_interfaces())
     secg = parse_secg_response(ec2.describe_security_groups())
 
-    q.put((region_name, keys, resv, secg))
+    q.put((region_name, keyp, resv, vols, neti, secg))
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser()
