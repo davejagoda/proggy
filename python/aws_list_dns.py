77720581d333fa9dce9723f70cd793e34cf7ce4d
dj <davejagoda+github@gmail.com>
2019-12-15 13:04:53 -0800

    Add details about ResourceRecordSets to aws_list_dns.py

--- python/aws_list_dns.py
+++ python/aws_list_dns.py
@@ -16,13 +16,31 @@ def print_domains(r53domains, verbose):
             domain['TransferLock'],
             domain['Expiry']))
 
-def print_zones(response, verbose):
+def print_record_sets(r53record_sets, verbose):
+    if verbose > 0:
+        print('Number of records found:{}'.format(len(r53record_sets['ResourceRecordSets'])))
+        pprint.pprint(r53record_sets)
+    print('Record Set')
+    for record_set in r53record_sets['ResourceRecordSets']:
+        print('{:20} {} {} {}'.format(
+            record_set['Name'],
+            record_set['ResourceRecords'],
+            record_set['TTL'],
+            record_set['Type']))
+
+def print_zones(zones, verbose):
     print('ZONES')
     if verbose > 0:
-        print('Number of zones found:{}'.format(len(response['HostedZones'])))
-        pprint.pprint(response)
-    for zone in response['HostedZones']:
-        print(zone['Name'])
+        print('Number of zones found:{}'.format(len(zones['HostedZones'])))
+        pprint.pprint(zones)
+    for zone in zones['HostedZones']:
+        print('{:20} {} {}'.format(
+            zone['Name'],
+            zone['Id'],
+            zone['ResourceRecordSetCount']))
+        response = boto3.client('route53', region_name='us-east-1')
+        print_record_sets(response.list_resource_record_sets(
+            HostedZoneId=zone['Id']), args.verbose)
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser()
