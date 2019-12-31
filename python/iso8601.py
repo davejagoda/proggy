c4b22cbe0c75952589ec8acb04ac7719bc269c6c
dj <davejagoda+github@gmail.com>
2019-12-31 12:33:53 -0800

    Modify iso8601.py to use now() with a timezone and remove trailing Z

--- python/iso8601.py
+++ python/iso8601.py
@@ -3,11 +3,13 @@
 import datetime, sys
 
 if 1 == len(sys.argv):
-    epoch = datetime.datetime.utcnow()
+    epoch = datetime.datetime.now(datetime.timezone.utc)
 else:
     try:
-        epoch = datetime.datetime.fromtimestamp(float(sys.argv[1]))
+        epoch = datetime.datetime.fromtimestamp(
+            float(sys.argv[1]), datetime.timezone.utc)
     except:
-        epoch = datetime.datetime.fromtimestamp(float(sys.argv[1])/1000.0)
+        epoch = datetime.datetime.fromtimestamp(
+            float(sys.argv[1])/1000.0, datetime.timezone.utc)
 
-print(epoch.replace(microsecond=0).isoformat()+'Z')
+print(epoch.replace(microsecond=0).isoformat())
