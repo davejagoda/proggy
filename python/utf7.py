29338d88e7e661f033045f93883524695bf53fe9
dj <davejagoda+github@gmail.com>
2021-01-17 09:01:24 -0800

    Add utf7.py

---
+++ python/utf7.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+def process_line(line):
+    line = line.rstrip()
+    print('{} => {}'.format(line, line.encode('utf7').decode('ascii')))
+
+if '__main__' == __name__:
+    current_file = None
+    for line in fileinput.input():
+        if fileinput.filename() != current_file:
+            current_file = fileinput.filename()
+            print('processing:{0}'.format(current_file))
+        process_line(line)
