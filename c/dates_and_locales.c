98a87aa7abc5bfebc5aff5c8f89e5995afcb248a
dj <davejagoda+github@gmail.com>
2021-08-28 14:55:31 +0000

    Add dates_and_locales.c

---
+++ c/dates_and_locales.c
@@ -0,0 +1,25 @@
+#include <locale.h>
+#include <stdio.h>
+#include <time.h>
+
+/*
+try these
+LANG=ja_JP.UTF-8 ./dates_and_locales
+LC_ALL=zh_CN.UTF-8 ./dates_and_locales
+LC_ALL=zh_TW.UTF-8 ./dates_and_locales
+LC_TIME=en_DK.UTF-8 ./dates_and_locales
+ */
+
+int main()
+{
+  time_t t = time(NULL);
+  struct tm *tmp;
+  tmp = localtime(&t);
+  char outstr[512];
+  char *current_locale;
+
+  current_locale = setlocale (LC_ALL, "");
+  printf("current locale is: %s\n", current_locale);
+  strftime(outstr, sizeof(outstr), "%x %X", tmp);
+  printf("%s\n", outstr);
+}
