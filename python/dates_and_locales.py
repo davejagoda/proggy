0b9b2a6927d2a9a6f5365783105db542753db30e
dj <davejagoda+github@gmail.com>
2021-09-04 10:34:28 -0700

    Modify dates_and_locales.py to setlocale otherwise default locale will be used

--- python/dates_and_locales.py
+++ python/dates_and_locales.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
+"""
+try this
+LANG=ja_JP.UTF-8 ./dates_and_locales.py
+"""
+
 import datetime
 import locale
 
+locale.setlocale(locale.LC_ALL, '')
+print(locale.getlocale())
 t = datetime.datetime.now()
 print(t.isoformat(timespec='seconds'))
 print(t.strftime('%x %X'))
-print(locale.getlocale())
