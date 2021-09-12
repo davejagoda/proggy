c7ce30d90d9c5d8a496ef08c224e92bee95150f7
dj <davejagoda+github@gmail.com>
2021-09-12 02:51:41 +0000

    Add more examples to dates_and_locales.py

--- python/dates_and_locales.py
+++ python/dates_and_locales.py
@@ -2,20 +2,23 @@
 
 """
 try this
-LANG=ja_JP.UTF-8 ./dates_and_locales.py
 
 OS X 11.5.2:
 grep -r %Y- /usr/share/locale
 /usr/share/locale/sv_SE.ISO8859-15/LC_TIME:%Y-%m-%d
 /usr/share/locale/sv_SE.ISO8859-1/LC_TIME:%Y-%m-%d
 
+LANG=ja_JP.UTF-8 ./dates_and_locales.py
 LANG=sv_SE.ISO8859-1 ./dates_and_locales.py
 
 Ubuntu 21.04:
-sudo apt-get install language-pack-ja
 sudo apt-get install language-pack-hi
+sudo apt-get install language-pack-ja
+sudo apt-get install language-pack-sv
 
 LANG=hi_IN.utf8 ./dates_and_locales.py
+LANG=ja_JP.utf8 ./dates_and_locales.py
+LANG=sv_SE.utf8 ./dates_and_locales.py
 """
 
 import datetime
