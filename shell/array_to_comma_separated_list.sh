43bbc9e7fd50adb8c56a601b7d04b0bfbfa7daf5
dj <davejagoda+github@gmail.com>
2023-03-04 16:10:43 -0800

    Add array_to_comma_separated_list.sh

---
+++ shell/array_to_comma_separated_list.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+vars=(
+    hoge
+    fuga
+    piyo
+)
+length=${#vars[@]}
+echo "length: ${length}"
+echo "first: ${vars[0]}"
+echo "last: ${vars[${length}-1]}"
+echo "here is the entire array:"
+for var in "${vars[@]}"
+do
+    echo "${var}"
+done
