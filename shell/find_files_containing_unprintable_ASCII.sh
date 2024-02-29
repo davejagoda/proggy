df750c022eb05b9c3fbf4ddfd7b9bf5b35d70384
dj <davejagoda+github@gmail.com>
2024-02-28 18:56:36 -0800

    Remove extra space in find_files_containing_unprintable_ASCII.sh

--- shell/find_files_containing_unprintable_ASCII.sh
+++ shell/find_files_containing_unprintable_ASCII.sh
@@ -2,4 +2,4 @@
 
 # https://unix.stackexchange.com/questions/350990/searching-files-containing-non-ascii-characters
 
-LC_ALL=C find . -type f -exec  grep -l '[^[:print:]]' {} \;
+LC_ALL=C find . -type f -exec grep -l '[^[:print:]]' {} \;
