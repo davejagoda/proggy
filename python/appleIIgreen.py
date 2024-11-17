277612f8fbdf1c85c168b09038f9f022ec2068f7
dj <davejagoda+github@gmail.com>
2024-11-17 15:20:22 -0800

    Add appleIIgreen.py

---
+++ python/appleIIgreen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+                                  R   G   B
+lt green COLOR=12 225 100   50     20 245  60
+green    HCOLOR=1 225 100   50     20 245  60
+dk green COLOR=4  270  60   25      0 163  96
+"""
+print('#{:02x}{:02x}{:02x}'.format(20, 245, 60))
+print('#{:02x}{:02x}{:02x}'.format( 0, 163, 96))
