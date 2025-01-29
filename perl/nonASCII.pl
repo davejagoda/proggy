d3662fb33030d201a99b7f4cff84780d7527a747
dj <davejagoda+github@gmail.com>
2025-01-28 18:56:00 -0800

    Modify nonASCII.pl to also read from STDIN

--- perl/nonASCII.pl
+++ perl/nonASCII.pl
@@ -1,11 +1,8 @@
 #!/usr/bin/perl
 
-foreach $file (@ARGV) {
-    open(my $fh, "<", $file)
-	or die "cannot open < $file: $!";
-    while (<$fh>) {
-	if (m/[[:^ascii:]]/) {
-	    print;
-	}
+
+while (<>) {
+    if (m/[[:^ascii:]]/) {
+        print;
     }
 }
