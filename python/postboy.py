6da32c57a14d03276e1aedba5953b2ce5016c82c
dj <davejagoda+github@gmail.com>
2025-01-31 19:10:29 -0800

    Add postboy.py

---
+++ python/postboy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+"""
+Remember back when Postman was a boy?
+"""
+
+import argparse
+import json
+import os
+
+import requests
+
+if "__main__" == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--api", required=True, help="Use this API key")
+    parser.add_argument("-j", "--json", required=True, help="POST this JSON")
+    parser.add_argument("-u", "--url", required=True, help="POST to this URL")
+    parser.add_argument(
+        "-v", "--verbosity", action="count", default=0, help="verbosity"
+    )
+    args = parser.parse_args()
+    headers = {"Content-Type": "application/json", "x-api-key": args.api}
+    with open(args.json) as f:
+        data = f.read()
+    response = requests.post(url=args.url, headers=headers, data=data)
+    print(f"HTTP status: {response.status_code}")
+    print(f"HTTP response text: {response.text}")
+    if args.verbosity > 1:
+        print(json.dumps(response.json(), indent=2))
