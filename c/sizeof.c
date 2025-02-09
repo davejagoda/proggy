0d4c9bbcbcbb3fb91d8daafa44fb741ff4f6edba
dj <davejagoda+github@gmail.com>
2025-02-08 19:31:54 -0800

    Add more types to sizeof.c
    Use OS X indent like this:
    
    indent -bl -i2 -npsl -nut sizeof.c

--- c/sizeof.c
+++ c/sizeof.c
@@ -6,4 +6,8 @@ int main()
   printf("sizeof(%s): %lu\n", "short", sizeof(short));
   printf("sizeof(%s): %lu\n", "int", sizeof(int));
   printf("sizeof(%s): %lu\n", "long", sizeof(long));
+  printf("sizeof(%s): %lu\n", "long long", sizeof(long long));
+  printf("sizeof(%s): %lu\n", "float", sizeof(float));
+  printf("sizeof(%s): %lu\n", "double", sizeof(double));
+  printf("sizeof(%s): %lu\n", "long double", sizeof(long double));
 }
