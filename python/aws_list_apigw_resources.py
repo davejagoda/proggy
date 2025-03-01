dedd7da5dd5ad6da6755f2a4904c7b5c5c73d90b
dj <davejagoda+github@gmail.com>
2025-03-01 13:06:50 -0800

    Add --region option to aws_list_apigw_resources.py

--- python/aws_list_apigw_resources.py
+++ python/aws_list_apigw_resources.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import os
 
 import boto3
 
@@ -9,7 +10,7 @@ LIMIT = 500
 
 
 # apigateway v1
-def get_rest_apis(client):
+def get_rest_apis(client, verbosity):
     items = []
     position = None
     while True:
@@ -17,6 +18,8 @@ def get_rest_apis(client):
             response = client.get_rest_apis(limit=LIMIT, position=position)
         else:
             response = client.get_rest_apis(limit=LIMIT)
+        if verbosity > 0:
+            print(json.dumps(response, indent=2, sort_keys=True))
         items.extend(response.get("items", []))
         position = response.get("position", None)
         if not position:
@@ -72,7 +75,7 @@ def print_last_v1_deployment_date(client, api_id):
 
 
 # apigateway v2
-def get_apis(client):
+def get_apis(client, verbosity):
     items = []
     next_token = None
     while True:
@@ -80,6 +83,8 @@ def get_apis(client):
             response = client.get_apis(NextToken=next_token)
         else:
             response = client.get_apis()
+        if verbosity > 0:
+            print(json.dumps(response, indent=2, sort_keys=True))
         items.extend(response.get("Items", []))
         next_token = response.get("NextToken", None)
         if not next_token:
@@ -91,14 +96,14 @@ def get_apis(client):
 
 if "__main__" == __name__:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--deployment_details", action="store_true")
-    parser.add_argument("-r", "--resource_details", action="store_true")
+    parser.add_argument("-d", "--details", action="store_true")
     parser.add_argument("-g", "--generic", action="store_true")
+    parser.add_argument("-r", "--region", default=os.getenv("AWS_REGION"))
     parser.add_argument("-v", "--verbosity", action="count", default=0)
     args = parser.parse_args()
     # aws apigateway get-rest-apis --output table
-    client = boto3.client("apigateway")
-    apis = get_rest_apis(client)
+    client = boto3.client("apigateway", region_name=args.region)
+    apis = get_rest_apis(client, args.verbosity)
     print("List of API Gateway Instances:")
     for api in apis:
         if args.verbosity > 0:
@@ -110,7 +115,7 @@ if "__main__" == __name__:
             print(
                 f"API ID: {api_id}, Name: {api.get('name')}, ProtocolType: REST, Description: {api.get('description', 'No Description')}"
             )
-        if args.resource_details:
+        if args.details:
             resources = get_resources(client, api_id)
             for resource in resources:
                 resource_id = resource.get("id")
@@ -123,11 +128,11 @@ if "__main__" == __name__:
                 else:
                     print(f"  Resource ID: {resource_id}, Path: {resource_path}")
                     print(json.dumps(resource, indent=2, sort_keys=True))
-        if args.deployment_details:
+        if args.details:
             print_last_v1_deployment_date(client, api_id)
     # aws apigatewayv2 get-apis --output table
-    client = boto3.client("apigatewayv2")
-    apis = get_apis(client)
+    client = boto3.client("apigatewayv2", region_name=args.region)
+    apis = get_apis(client, args.verbosity)
     print("List of API GatewayV2 Instances:")
     for api in apis:
         if args.generic:
