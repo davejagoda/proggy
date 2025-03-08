e240409bbc29bc7088e52538cdfbe647ed443c17
dj <davejagoda+github@gmail.com>
2025-03-07 16:32:28 -0800

    Make API key optional and redirects configurable

--- python/postboy.py
+++ python/postboy.py
@@ -13,18 +13,33 @@ import requests
 
 if "__main__" == __name__:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--api", required=True, help="Use this API key")
+    parser.add_argument("-a", "--api", default=None, help="Use this API key")
     parser.add_argument("-j", "--json", required=True, help="POST this JSON")
+    parser.add_argument(
+        "-r", "--redirect", default=False, action="store_true", help="Follow redirects"
+    )
     parser.add_argument("-u", "--url", required=True, help="POST to this URL")
     parser.add_argument(
         "-v", "--verbosity", action="count", default=0, help="verbosity"
     )
     args = parser.parse_args()
-    headers = {"Content-Type": "application/json", "x-api-key": args.api}
+    headers = {"Content-Type": "application/json"}
+    if args.api:
+        headers["x-api-key"] = args.api
+    if args.verbosity > 1:
+        print(headers)
     with open(args.json) as f:
         data = f.read()
-    response = requests.post(url=args.url, headers=headers, data=data)
+    response = requests.post(
+        url=args.url, headers=headers, data=data, allow_redirects=args.redirect
+    )
+    if response.history:
+        for item in response.history:
+            print(item.status_code, item.url)
     print(f"HTTP status: {response.status_code}")
     print(f"HTTP response text: {response.text}")
-    if args.verbosity > 1:
-        print(json.dumps(response.json(), indent=2, sort_keys=True))
+    if args.verbosity > 0:
+        try:
+            print(json.dumps(response.json(), indent=2, sort_keys=True))
+        except requests.JSONDecodeError:
+            print("Could not decode the response text into json")
