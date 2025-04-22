837a666cbb1464a0703bcfb7b8753355cda0e82b
dj <davejagoda+github@gmail.com>
2025-04-22 12:14:24 -0700

    Remove trailing whitespace in aws_block_public_access_to_s3_buckets.sh

--- shell/aws_block_public_access_to_s3_buckets.sh
+++ shell/aws_block_public_access_to_s3_buckets.sh
@@ -5,5 +5,5 @@ set -Eeuo pipefail
 for bucket in $(aws s3api list-buckets --output text | grep BUCKETS | cut -f 3)
 do
     echo $bucket
-    aws s3api put-public-access-block --bucket $bucket --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true" 
+    aws s3api put-public-access-block --bucket $bucket --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
 done
