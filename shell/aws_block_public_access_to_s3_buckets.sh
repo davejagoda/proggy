bc7123fb294e03a38e8ea51a4851d17892e51497
dj <davejagoda+github@gmail.com>
2025-04-22 11:56:14 -0700

    Add aws_block_public_access_to_s3_buckets.sh

---
+++ shell/aws_block_public_access_to_s3_buckets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+for bucket in $(aws s3api list-buckets --output text | grep BUCKETS | cut -f 3)
+do
+    echo $bucket
+    aws s3api put-public-access-block --bucket $bucket --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true" 
+done
