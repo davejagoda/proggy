6c65bda142c43299471c0ba6562e6bd2e1309751
dj <davejagoda+github@gmail.com>
2025-09-11 18:41:59 -0700

    Update uniq_words.js to use import instead of require

--- javascript/uniq_words.js
+++ javascript/uniq_words.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
+import { readFile } from 'node:fs';
 
 if (3 !== process.argv.length) {
   console.log("Provide exactly one argument: a file to read");
@@ -10,7 +10,7 @@ if (3 !== process.argv.length) {
 const file = process.argv[2];
 let uniq = {};
 
-fs.readFile(file, "utf8", function (err, data) {
+readFile(file, "utf8", function (err, data) {
   if (err) {
     return console.log(err);
   }
