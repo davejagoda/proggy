a2e959fcc290925b96a4b5af0c2d312365bd4826
dj <davejagoda+github@gmail.com>
2025-09-21 11:48:39 -0700

    Make verbosity a count-type argument in argparsy.py

--- python/argparsy.py
+++ python/argparsy.py
@@ -6,11 +6,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("source", nargs="+", help="the source[s]")
 parser.add_argument("destination", nargs=1, help="the destination directory")
 # mv [-f | -i | -n] [-v] source ... directory
-parser.add_argument("-v", "--verbose", action="store_true", help="show verbose output")
+parser.add_argument("-v", "--verbosity", action="count", default=0, help="verbosity")
 args = parser.parse_args()
-if args.verbose:
+if args.verbosity > 0:
     print("about to echo arguments")
 print("source[s]:{}".format(args.source))
 print("destination:{}".format(args.destination))
-if args.verbose:
+if args.verbosity > 0:
     print("just echoed arguments")
