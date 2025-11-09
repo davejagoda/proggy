229549d50ba26f538c9b91e2b46c59caa24bdcca
dj <davejagoda+github@gmail.com>
2025-11-09 07:02:57 -0800

    Handle Unix time in milliseconds in epoch2iso8601.py

--- python/epoch2iso8601.py
+++ python/epoch2iso8601.py
@@ -3,5 +3,12 @@
 import datetime
 import sys
 
-for date in sys.argv[1:]:
-    print(datetime.datetime.isoformat(datetime.datetime.fromtimestamp(float(date))))
+for unix_time in sys.argv[1:]:
+    # check for time in milliseconds, will break after year 9999
+    if 13 == len(unix_time):
+        unix_time = unix_time[:10]
+    print(
+        datetime.datetime.isoformat(
+            datetime.datetime.fromtimestamp(float(unix_time), datetime.UTC)
+        )
+    )
