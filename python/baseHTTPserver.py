186e1f6c69c2c26a62ccc341d8e73e43b7504718
dj <davejagoda+github@gmail.com>
2025-12-02 17:15:50 -0800

    Update baseHTTPserver.py to use python3

--- python/baseHTTPserver.py
+++ python/baseHTTPserver.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 # https://www.piware.de/2011/01/creating-an-https-server-in-python/
 # make self-signed cert:
-# openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
+# openssl req -new -x509 -keyout localhost.pem -out localhost.pem -days 365 -nodes
 
 import argparse
 import ssl
-
-import BaseHTTPServer
-import SimpleHTTPServer
-
-
-class djHTTPServer(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    pass
-
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 if "__main__" == __name__:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", required=True, help="listen port")
+    parser.add_argument("-p", "--port", required=True, type=int, help="listen port")
     parser.add_argument("-s", "--ssl", action="store_true", help="SSL")
     parser.add_argument("-v", "--verbose", action="store_true", help="be verbose")
     args = parser.parse_args()
-    httpd = BaseHTTPServer.HTTPServer(("", int(args.port)), djHTTPServer)
-    if args.ssl:
-        httpd.socket = ssl.wrap_socket(
-            httpd.socket, certfile="localhost.pem", server_side=True
-        )
-    httpd.serve_forever()
+    with HTTPServer(("", args.port), SimpleHTTPRequestHandler) as httpd:
+        if args.ssl:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            context.load_cert_chain(certfile="localhost.pem")
+            context.check_hostname = False
+            httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+        httpd.serve_forever()
