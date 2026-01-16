3760f404fb9b71c4402491e8a27311c213c23442
dj <davejagoda+github@gmail.com>
2026-01-15 19:13:00 -0800

    Remove unneeded code from sort_on_line_length.py

--- python/sort_on_line_length.py
+++ python/sort_on_line_length.py
@@ -3,7 +3,5 @@
 # https://stackoverflow.com/questions/5917576/sort-a-text-file-by-line-length-including-spaces/71484419#71484419
 
 import fileinput
-import sys
 
-# sys.stdout.writelines(sorted(fileinput.input(), key=len))
 print("".join(sorted(fileinput.input(), key=len)), end="")
